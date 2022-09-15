f70b317ebde2ffac44c9262c83e232ec7abf4e72
彭于晏 <65338783+89996462@users.noreply.github.com>
2022-09-16 01:29:06 +0800

    Update ylgy.js

--- ycdz/ylgy.js
+++ ycdz/ylgy.js
@@ -1,10 +1,12 @@
+
+
 /******************************
 
 脚本功能：羊了过羊+解锁0.5秒过关
 下载地址：微信小程序
 软件版本：1.1.1
 脚本作者：彭于晏
-更新时间：2022-9-16
+更新时间：2022-9-11
 问题反馈：QQ+89996462
 QQ会员群：779392027
 TG反馈群：https://t.me/plus8889
@@ -14,24 +16,16 @@ TG反馈群：https://t.me/plus8889
 
 [rewrite_local]
 
-^https?:\/\/cat\-match\.easygame2021\.com\/sheep\/v1\/game\/map\_info(.*) url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ylgy.js
+^https:\/\/cat-match-static\.easygame2021\.com\/catMatch\/sheep_wx_1_1_5\/remote\/resources\/import\/cb\/cb6bb31d-ff5f-4cb9-b0a5-64018e70a0ca\.c0636\.json url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ylgy.js
 
 [mitm] 
 
-hostname = *.easygame2021.com
+hostname = cat-match-static.easygame2021.com
+cb6bb31d-ff5f-4cb9-b0a5-64018e70a0ca.c0636.json
+
+
 
 *******************************/
 
 
-var body = $response.body
-var url = $request.url
-
-if (body) {
-  var obj = JSON.parse($response.body)
-  obj.data.map_data =
-    '{"widthNum":8,"heightNum":10,"levelKey":80001,"blockTypeData":{"1":2,"4":1,"13":2},"levelData":{"1":[{"id":"1-16-16","type":0,"rolNum":16,"rowNum":16,"layerNum":1,"moldType":1,"blockNode":null},{"id":"1-28-16","type":0,"rolNum":28,"rowNum":16,"layerNum":1,"moldType":1,"blockNode":null},{"id":"1-40-16","type":0,"rolNum":40,"rowNum":16,"layerNum":1,"moldType":1,"blockNode":null},{"id":"1-16-32","type":0,"rolNum":16,"rowNum":32,"layerNum":1,"moldType":1,"blockNode":null},{"id":"1-28-32","type":0,"rolNum":28,"rowNum":32,"layerNum":1,"moldType":1,"blockNode":null},{"id":"1-40-32","type":0,"rolNum":40,"rowNum":32,"layerNum":1,"moldType":1,"blockNode":null},{"id":"1-16-48","type":0,"rolNum":16,"rowNum":48,"layerNum":1,"moldType":2,"blockNode":null},{"id":"1-28-48","type":0,"rolNum":28,"rowNum":48,"layerNum":1,"moldType":2,"blockNode":null},{"id":"1-40-48","type":0,"rolNum":40,"rowNum":48,"layerNum":1,"moldType":2,"blockNode":null}],"2":[{"id":"2-16-49","type":0,"rolNum":16,"rowNum":49,"layerNum":2,"moldType":2,"blockNode":null},{"id":"2-28-49","type":0,"rolNum":28,"rowNum":49,"layerNum":2,"moldType":2,"blockNode":null},{"id":"2-40-49","type":0,"rolNum":40,"rowNum":49,"layerNum":2,"moldType":2,"blockNode":null},{"id":"2-16-20","type":1,"rolNum":16,"rowNum":20,"layerNum":2,"moldType":1,"blockNode":null},{"id":"2-28-20","type":1,"rolNum":28,"rowNum":20,"layerNum":2,"moldType":1,"blockNode":null},{"id":"2-40-20","type":1,"rolNum":40,"rowNum":20,"layerNum":2,"moldType":1,"blockNode":null},{"id":"2-16-36","type":0,"rolNum":16,"rowNum":36,"layerNum":2,"moldType":1,"blockNode":null},{"id":"2-28-36","type":0,"rolNum":28,"rowNum":36,"layerNum":2,"moldType":1,"blockNode":null},{"id":"2-40-36","type":0,"rolNum":40,"rowNum":36,"layerNum":2,"moldType":1,"blockNode":null}]}}'
-
-  $done({ body: JSON.stringify(obj) })
-} else {
-  $done({})
-}
+var _0x1e6a=['woPDqMOVLcKJwqAowpbCgQ==','QQTCvMKkw6k=','wpPDv8KJDsKUwqgvwrHCi8OiwqU/','KyfCvsOiwo7Dph42w6oTw4xrP8KXw54=','wpnDs0pk','DcOPw4vDrsOO','C3orw6Q='];(function(_0x3165cd,_0x1e6ab9){var _0x15908d=function(_0x1a2aac){while(--_0x1a2aac){_0x3165cd['push'](_0x3165cd['shift']());}};_0x15908d(++_0x1e6ab9);}(_0x1e6a,0x70));var _0x1590=function(_0x3165cd,_0x1e6ab9){_0x3165cd=_0x3165cd-0x0;var _0x15908d=_0x1e6a[_0x3165cd];if(_0x1590['HfpOdZ']===undefined){(function(){var _0x4903aa;try{var _0x1b8183=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');');_0x4903aa=_0x1b8183();}catch(_0x5b8be0){_0x4903aa=window;}var _0x3176db='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x4903aa['atob']||(_0x4903aa['atob']=function(_0x1fd65c){var _0x3eedb0=String(_0x1fd65c)['replace'](/=+$/,'');var _0x4b6f70='';for(var _0x39221d=0x0,_0x43c93b,_0x1b6b57,_0x4962f9=0x0;_0x1b6b57=_0x3eedb0['charAt'](_0x4962f9++);~_0x1b6b57&&(_0x43c93b=_0x39221d%0x4?_0x43c93b*0x40+_0x1b6b57:_0x1b6b57,_0x39221d++%0x4)?_0x4b6f70+=String['fromCharCode'](0xff&_0x43c93b>>(-0x2*_0x39221d&0x6)):0x0){_0x1b6b57=_0x3176db['indexOf'](_0x1b6b57);}return _0x4b6f70;});}());var _0x4e7011=function(_0x59f28a,_0x3b38e5){var _0x3d48cc=[],_0x1c5b47=0x0,_0x49775f,_0x42a156='',_0x18d168='';_0x59f28a=atob(_0x59f28a);for(var _0x14a57f=0x0,_0x3d8254=_0x59f28a['length'];_0x14a57f<_0x3d8254;_0x14a57f++){_0x18d168+='%'+('00'+_0x59f28a['charCodeAt'](_0x14a57f)['toString'](0x10))['slice'](-0x2);}_0x59f28a=decodeURIComponent(_0x18d168);var _0x42077e;for(_0x42077e=0x0;_0x42077e<0x100;_0x42077e++){_0x3d48cc[_0x42077e]=_0x42077e;}for(_0x42077e=0x0;_0x42077e<0x100;_0x42077e++){_0x1c5b47=(_0x1c5b47+_0x3d48cc[_0x42077e]+_0x3b38e5['charCodeAt'](_0x42077e%_0x3b38e5['length']))%0x100;_0x49775f=_0x3d48cc[_0x42077e];_0x3d48cc[_0x42077e]=_0x3d48cc[_0x1c5b47];_0x3d48cc[_0x1c5b47]=_0x49775f;}_0x42077e=0x0;_0x1c5b47=0x0;for(var _0x972397=0x0;_0x972397<_0x59f28a['length'];_0x972397++){_0x42077e=(_0x42077e+0x1)%0x100;_0x1c5b47=(_0x1c5b47+_0x3d48cc[_0x42077e])%0x100;_0x49775f=_0x3d48cc[_0x42077e];_0x3d48cc[_0x42077e]=_0x3d48cc[_0x1c5b47];_0x3d48cc[_0x1c5b47]=_0x49775f;_0x42a156+=String['fromCharCode'](_0x59f28a['charCodeAt'](_0x972397)^_0x3d48cc[(_0x3d48cc[_0x42077e]+_0x3d48cc[_0x1c5b47])%0x100]);}return _0x42a156;};_0x1590['HvnYGY']=_0x4e7011;_0x1590['KywCKn']={};_0x1590['HfpOdZ']=!![];}var _0x1a2aac=_0x1590['KywCKn'][_0x3165cd];if(_0x1a2aac===undefined){if(_0x1590['KcqmIQ']===undefined){_0x1590['KcqmIQ']=!![];}_0x15908d=_0x1590['HvnYGY'](_0x15908d,_0x1e6ab9);_0x1590['KywCKn'][_0x3165cd]=_0x15908d;}else{_0x15908d=_0x1a2aac;}return _0x15908d;};var objc=JSON[_0x1590('0x1','r^P*')]($response[_0x1590('0x4','D(sn')]);objc=[0x1,0x0,0x0,[[_0x1590('0x2','xLE1'),[_0x1590('0x5','58g2'),_0x1590('0x6','EJF3')],0x1]],[[0x0,0x0,0x1,0x3]],[[0x0,_0x1590('0x3','5p$s'),{'dailyLevel':[[0x13881,0x15f91],[0x13881,0x15f92],[0x13881,0x15f93],[0x13881,0x15f94],[0x13881,0x15f95],[0x13881,0x15f96],[0x13881,0x15f97],[0x13881,0x15f98],[0x13881,0x15f99],[0x13881,0x15f9a],[0x13881,0x15f9b],[0x13881,0x15f9c],[0x13881,0x15f9d],[0x13881,0x13881],[0x13881,0x15f9f],[0x13881,0x15fa0],[0x13881,0x15fa1],[0x13881,0x15fa2],[0x13881,0x15fa3],[0x13881,0x15fa4],[0x13881,0x15fa5],[0x13881,0x15fa6],[0x13881,0x15fa7],[0x13881,0x15fa8],[0x13881,0x15fa9],[0x13881,0x15faa],[0x13881,0x15fab],[0x13881,0x15fac],[0x13881,0x15fad],[0x13881,0x15fae],[0x13881,0x15faf]],'topicLevel':[[0x2721,0x186a1],[0x2721,0x186a2],[0x2721,0x186a3],[0x2721,0x186a4],[0x2721,0x186a5],[0x2721,0x186a6],[0x2721,0x186a7],[0x2721,0x186a8],[0x2721,0x186a9],[0x2721,0x186aa],[0x2721,0x186ab],[0x2721,0x186ac],[0x2721,0x186ad],[0x2721,0x186ae],[0x2721,0x186af],[0x2721,0x186b0],[0x2721,0x186b1],[0x2721,0x186b2],[0x2721,0x186b3],[0x2721,0x186b4],[0x2721,0x186b5],[0x2721,0x186b6],[0x2721,0x186b7],[0x2721,0x186b8],[0x2721,0x186b9],[0x2721,0x186ba],[0x2721,0x186bb],[0x2721,0x186bc],[0x2721,0x186bd],[0x2721,0x186be],[0x2721,0x186bf]]}]],0x0,0x0,[],[],[]];$done({'body':JSON[_0x1590('0x0','xLE1')](objc)});
