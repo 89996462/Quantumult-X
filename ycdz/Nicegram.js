241d77d90523aa1a04d86d35966d7bd8303a01fe
彭于晏 <65338783+89996462@users.noreply.github.com>
2022-10-08 18:52:40 +0800

    Update Nicegram.js

--- ycdz/Nicegram.js
+++ ycdz/Nicegram.js
@@ -1,14 +1,3 @@
-
-[rewrite_local]
-
-https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Nicegram.js
-
-[mitm] 
-
-hostname = restore-access.indream.app
-
-
-
 {
   "data" : {
     "premiumAccess" : true
