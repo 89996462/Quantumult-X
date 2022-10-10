1eb5195a61d61a5338cf7495138e6cd99e0035ce
彭于晏 <65338783+89996462@users.noreply.github.com>
2022-10-10 12:03:37 +0800

    Update 91tv.js

--- ghs/91tv.js
+++ ghs/91tv.js
@@ -4,7 +4,7 @@
 软件下载：https://app.glvr.xyz/i-WG4S5YY
 软件版本：v666
 脚本作者：彭于晏💞
-更新时间：2022-10-5
+更新时间：2022-10-9
 问题反馈：QQ+89996462
 QQ会员群：779392027💞
 TG反馈群：https://t.me/plus8889
@@ -28,6 +28,6 @@ hostname = appapi.*.com,api.*.com
 
 var modifiedHeaders = $request.headers;
 
-modifiedHeaders['X-AUTH-TOKEN'] = 'eyJhbGciOiJIUzUxMiIsImlhdCI6MTY2MzgxNzEyMCwiZXhwIjoxNjc5MzY5MTIwfQ.eyJpZCI6Njc3MzY5MzJ9.hg0oelDvuFT1Glu7t5GwgEiFu1es-bxbiFMq2qOAj7aoCTHR3MbEiPxji7oYIQWWi9y5SwhBK6lZnGHukcYQuA';
+modifiedHeaders['X-AUTH-TOKEN'] = 'eyJhbGciOiJIUzUxMiIsImlhdCI6MTY2NTMzODQwMywiZXhwIjoxNjgwODkwNDAzfQ.eyJpZCI6ODY0NDExNzl9.ccqW6kKRAtEP5ylxG-pL53bCZfOeKQpaRnypZ8Ez2rkfaTj_2ZKCbQwH8WTDJYYVvGZrTXfr_pW6JwCooj6yMg';
 
 $done({headers : modifiedHeaders});
