032b9474751b99f10b813f6e0201149da2ab082f
彭于晏 <65338783+89996462@users.noreply.github.com>
2022-10-17 19:05:41 +0800

    Update CaiXin.js

--- gx/CaiXin.js
+++ gx/CaiXin.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/gateway\.caixin\.com\/api\/ucenter\/appapi\/v1\/userinfo url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/gx/CaiXin.js
+^https:\/\/gateway\.caixin\.com\/api\/ucenter\/appapi\/v1\/userinfo url script-request-header https://raw.githubusercontent.com/89996462/Quantumult-X/main/gx/CaiXin.js
 
 [mitm] 
 
