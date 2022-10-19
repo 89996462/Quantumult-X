5fd3c3c421ede9e422324b80e08b11fd93c96a6d
彭于晏 <65338783+89996462@users.noreply.github.com>
2022-10-19 11:42:30 +0800

    Create ddqd.js

---
+++ ycdz/ddqd.js
@@ -0,0 +1,26 @@
+/******************************
+
+脚本功能：滴答清单+解锁VIP
+下载地址：https://is.gd/xDbsrm
+软件版本：6.3.80
+脚本作者：彭于晏💞
+更新时间：2022-10-19
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/dida365\.com\/api\/v2\/user\/status url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ddqd.js
+
+[mitm] 
+
+hostname = dida365.com
+
+*******************************/
+var _0xf36b=["\x62\x6F\x64\x79","\x70\x61\x72\x73\x65","\x70\x72\x6F\x45\x6E\x64\x44\x61\x74\x65","\x32\x30\x39\x39\x2D\x30\x31\x2D\x30\x31\x54\x30\x30\x3A\x30\x30\x3A\x30\x30\x2E\x30\x30\x30\x2B\x30\x30\x30\x30","\x6E\x65\x65\x64\x53\x75\x62\x73\x63\x72\x69\x62\x65","\x70\x72\x6F","\x73\x74\x72\x69\x6E\x67\x69\x66\x79"];var body=$response[_0xf36b[0]];var obj=JSON[_0xf36b[1]](body);obj[_0xf36b[2]]= _0xf36b[3];obj[_0xf36b[4]]= false;obj[_0xf36b[5]]= true;body= JSON[_0xf36b[6]](obj);$done(body)
+
