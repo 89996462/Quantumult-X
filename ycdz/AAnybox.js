58d8e968d435c48a083b3cba09c55cdfb2ffed73
彭于晏 <65338783+89996462@users.noreply.github.com>
2022-10-31 23:15:15 +0800

    Update AAnybox.js

--- ycdz/AAnybox.js
+++ ycdz/AAnybox.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/AAnybox.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/(.*?)*$) url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/AAnybox.js
 
 [mitm] 
 
