83e2294b174b27525abbdb3c93831de53fceae25
彭于晏 <65338783+89996462@users.noreply.github.com>
2022-11-06 23:03:36 +0800

    Update ymjz.js

--- ycdz/ymjz.js
+++ ycdz/ymjz.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/yimuapp\.com:8082\/bookkeeping\/user\/getUser\/ofmJVs7nZxzRnnPUe9530AMrwEmc url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ymjz.js
+^https:\/\/yimuapp\.com:8082\/bookkeeping\/user\/getUser url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ymjz.js
 
 [mitm] 
 
