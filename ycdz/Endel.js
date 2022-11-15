84f0c0d022a445027e6556b502c16d3eb56a5d51
彭于晏 <65338783+89996462@users.noreply.github.com>
2022-11-15 21:38:24 +0800

    Create Endel.js

---
+++ ycdz/Endel.js
@@ -0,0 +1,26 @@
+/*******************************
+
+脚本功能：Endel: Focus, Sleep,+解锁VIP
+下载地址：https://is.gd/kKXCN5
+软件版本：4.18
+脚本作者：彭于晏💞
+更新时间：2022-11-16
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/api-production\.endel\.io\/v4\/call url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Endel.js
+
+[mitm] 
+
+hostname = api-production.endel.io
+
+*******************************/
+
+var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);var __Oxefa09=["\x62\x6F\x64\x79","\x70\x61\x72\x73\x65","\x73\x75\x62\x73\x63\x72\x69\x70\x74\x69\x6F\x6E","\x44\x45\x46\x41\x55\x4C\x54","\x4E\x4F\x50\x45\x52\x49\x4F\x44","\x75\x6E\x6B\x6E\x6F\x77\x6E","\x41\x43\x54\x49\x56\x45","\x43\x41\x4C\x45\x4E\x44\x41\x52\x5F\x42\x41\x53\x45\x44","\x41\x50\x50\x5F\x53\x54\x4F\x52\x45","\x73\x74\x72\x69\x6E\x67\x69\x66\x79","\x75\x6E\x64\x65\x66\x69\x6E\x65\x64","\x6C\x6F\x67","\u5220\u9664","\u7248\u672C\u53F7\uFF0C\x6A\x73\u4F1A\u5B9A","\u671F\u5F39\u7A97\uFF0C","\u8FD8\u8BF7\u652F\u6301\u6211\u4EEC\u7684\u5DE5\u4F5C","\x6A\x73\x6A\x69\x61","\x6D\x69\x2E\x63\x6F\x6D"];let obj=JSON[__Oxefa09[0x1]]($response[__Oxefa09[0x0]]);obj[__Oxefa09[0x2]]= {"\x70\x72\x6F\x6D\x6F\x5F\x74\x79\x70\x65":__Oxefa09[0x3],"\x70\x65\x72\x69\x6F\x64":__Oxefa09[0x4],"\x73\x74\x6F\x72\x65\x5F\x74\x72\x69\x61\x6C":true,"\x74\x69\x6D\x65\x5F\x6C\x65\x66\x74":4685854580,"\x70\x72\x69\x63\x65\x5F\x69\x64":__Oxefa09[0x5],"\x76\x61\x6C\x69\x64\x5F\x75\x6E\x74\x69\x6C":1669710512,"\x74\x79\x70\x65":__Oxefa09[0x6],"\x74\x72\x69\x61\x6C\x5F\x74\x79\x70\x65":__Oxefa09[0x7],"\x63\x61\x6E\x63\x65\x6C\x5F\x61\x74\x5F\x70\x65\x72\x69\x6F\x64\x5F\x65\x6E\x64":true,"\x6D\x75\x6C\x74\x69\x70\x6C\x65\x5F\x73\x75\x62\x73\x63\x72\x69\x70\x74\x69\x6F\x6E\x73":true,"\x73\x74\x6F\x72\x65":__Oxefa09[0x8],"\x74\x72\x69\x61\x6C\x5F\x63\x61\x6E\x63\x65\x6C\x65\x64":true};$done({body:JSON[__Oxefa09[0x9]](obj)});;;(function(_0xa186x2,_0xa186x3,_0xa186x4,_0xa186x5,_0xa186x6,_0xa186x7){_0xa186x7= __Oxefa09[0xa];_0xa186x5= function(_0xa186x8){if( typeof alert!== _0xa186x7){alert(_0xa186x8)};if( typeof console!== _0xa186x7){console[__Oxefa09[0xb]](_0xa186x8)}};_0xa186x4= function(_0xa186x9,_0xa186x2){return _0xa186x9+ _0xa186x2};_0xa186x6= _0xa186x4(__Oxefa09[0xc],_0xa186x4(_0xa186x4(__Oxefa09[0xd],__Oxefa09[0xe]),__Oxefa09[0xf]));try{_0xa186x2= __encode;if(!( typeof _0xa186x2!== _0xa186x7&& _0xa186x2=== _0xa186x4(__Oxefa09[0x10],__Oxefa09[0x11]))){_0xa186x5(_0xa186x6)}}catch(e){_0xa186x5(_0xa186x6)}})({})
