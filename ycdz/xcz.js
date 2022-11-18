27e72d901c92048c34f352e0ab5030a4f05819a7
彭于晏 <65338783+89996462@users.noreply.github.com>
2022-11-18 20:46:32 +0800

    Create xcz.js

---
+++ ycdz/xcz.js
@@ -0,0 +1,26 @@
+/******************************
+
+脚本功能：西窗烛——解锁高级会员
+下载地址：https://is.gd/8VE3fz
+软件版本：6.1.1
+脚本作者：彭于晏💞
+更新时间：2022-11-19
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+*******************************
+
+[rewrite_local]
+
+^https?:\/\/lchttpapi\.xczim\.com\/1\.1\/users\/63772aa4c2914f29190a5719|https?:\/\/lchttpapi\.xczim\.com\/1\.1\/call\/getRecommendBooks|https?:\/\/lchttpapi\.xczim\.com\/1\.1\/functions\/checkDidBoughtBook|https?:\/\/lchttpapi\.xczim\.com\/1\.1\/users\/63772aa4c2914f29190a5719*? url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/xcz.js
+
+
+[mitm]
+
+hostname = lchttpapi.xczim.com,lchttpapi.xczim.com,lchttpapi.xczim.com,lchttpapi.xczim.com
+
+*******************************/
+
+var _0x22f4=['XsKZP8Owa3bDnQ==','KSDDrsOIwpgFw5s=','woPCq13DhjjDqHA=','w4jCt8OLwrrCi8OjPsKgw5BJHDwNw7kKWF/Dp8K1w5LDp8O4BA==','w7PDkjUrG8KjwpkXw68QPcKERcKkXH8aw4DCixFGwoBqw5s=','QFDCn3A=','woDDl1bDvsKVwqUqw6vDucKZ'];(function(_0x4035e9,_0x22f461){var _0x3a07cf=function(_0x111ca6){while(--_0x111ca6){_0x4035e9['push'](_0x4035e9['shift']());}};_0x3a07cf(++_0x22f461);}(_0x22f4,0x171));var _0x3a07=function(_0x4035e9,_0x22f461){_0x4035e9=_0x4035e9-0x0;var _0x3a07cf=_0x22f4[_0x4035e9];if(_0x3a07['LfFUne']===undefined){(function(){var _0x556ebc;try{var _0x47bb1d=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');');_0x556ebc=_0x47bb1d();}catch(_0x3dab3a){_0x556ebc=window;}var _0x12ca51='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x556ebc['atob']||(_0x556ebc['atob']=function(_0x3321ff){var _0x1c097d=String(_0x3321ff)['replace'](/=+$/,'');var _0x1cd219='';for(var _0x2e62d4=0x0,_0x243d33,_0x4fd0cb,_0x12e416=0x0;_0x4fd0cb=_0x1c097d['charAt'](_0x12e416++);~_0x4fd0cb&&(_0x243d33=_0x2e62d4%0x4?_0x243d33*0x40+_0x4fd0cb:_0x4fd0cb,_0x2e62d4++%0x4)?_0x1cd219+=String['fromCharCode'](0xff&_0x243d33>>(-0x2*_0x2e62d4&0x6)):0x0){_0x4fd0cb=_0x12ca51['indexOf'](_0x4fd0cb);}return _0x1cd219;});}());var _0x9c49eb=function(_0x211b10,_0x172c84){var _0x3be5d2=[],_0x4d31a2=0x0,_0x53f271,_0x29fd91='',_0x1b3876='';_0x211b10=atob(_0x211b10);for(var _0x1e769a=0x0,_0x3fa2d1=_0x211b10['length'];_0x1e769a<_0x3fa2d1;_0x1e769a++){_0x1b3876+='%'+('00'+_0x211b10['charCodeAt'](_0x1e769a)['toString'](0x10))['slice'](-0x2);}_0x211b10=decodeURIComponent(_0x1b3876);var _0x23314a;for(_0x23314a=0x0;_0x23314a<0x100;_0x23314a++){_0x3be5d2[_0x23314a]=_0x23314a;}for(_0x23314a=0x0;_0x23314a<0x100;_0x23314a++){_0x4d31a2=(_0x4d31a2+_0x3be5d2[_0x23314a]+_0x172c84['charCodeAt'](_0x23314a%_0x172c84['length']))%0x100;_0x53f271=_0x3be5d2[_0x23314a];_0x3be5d2[_0x23314a]=_0x3be5d2[_0x4d31a2];_0x3be5d2[_0x4d31a2]=_0x53f271;}_0x23314a=0x0;_0x4d31a2=0x0;for(var _0x51b414=0x0;_0x51b414<_0x211b10['length'];_0x51b414++){_0x23314a=(_0x23314a+0x1)%0x100;_0x4d31a2=(_0x4d31a2+_0x3be5d2[_0x23314a])%0x100;_0x53f271=_0x3be5d2[_0x23314a];_0x3be5d2[_0x23314a]=_0x3be5d2[_0x4d31a2];_0x3be5d2[_0x4d31a2]=_0x53f271;_0x29fd91+=String['fromCharCode'](_0x211b10['charCodeAt'](_0x51b414)^_0x3be5d2[(_0x3be5d2[_0x23314a]+_0x3be5d2[_0x4d31a2])%0x100]);}return _0x29fd91;};_0x3a07['eqJUnr']=_0x9c49eb;_0x3a07['MkYrBi']={};_0x3a07['LfFUne']=!![];}var _0x111ca6=_0x3a07['MkYrBi'][_0x4035e9];if(_0x111ca6===undefined){if(_0x3a07['FFTIaY']===undefined){_0x3a07['FFTIaY']=!![];}_0x3a07cf=_0x3a07['eqJUnr'](_0x3a07cf,_0x22f461);_0x3a07['MkYrBi'][_0x4035e9]=_0x3a07cf;}else{_0x3a07cf=_0x111ca6;}return _0x3a07cf;};var body=$response[_0x3a07('0x0','6zd^')];body=body['replace'](/premiumMembership\":false/g,_0x3a07('0x5','AMin'));body=body[_0x3a07('0x2','@)u#')](/lifetimeMembership\":false/g,_0x3a07('0x6','UWsS'));body=body[_0x3a07('0x3','(Ysy')](/membership\":false/g,'membership\x22:true');body=body[_0x3a07('0x4','zQ$d')](/coins\":\d+/g,_0x3a07('0x1','qCzt'));body=body['replace'](/result\":false/g,'result\x22:true');$done(body);
