1423bcadbfa099e5e6e1a285843b0ca743ee7b64
彭于晏 <65338783+89996462@users.noreply.github.com>
2022-11-26 13:17:16 +0800

    Update sssp.js

--- ghs/sssp.js
+++ ghs/sssp.js
@@ -26,10 +26,10 @@ hostname = files.yuchenglw.com
 
 
 var body=$response.body;
-body = body.replace(/(<div class="banner">)[\s\S[\d\D]{0,1666}(<\/div>)/g,'<!--  -->');
-
 
 body = body.replace(/(<div class="speaker">)[\s\S[\d\D]{0,345}(<\/div>)/g,'<!--  -->');
+
+   
 
 
 body = body.replace(/(<div id="popup">)[\s\S[\d\D]{0,1539}(<\/div>)/g,'<!--  -->');
@@ -45,7 +45,15 @@ body = body.replace(/(<div id="carousel")[\s\S[\d\D]{0,1777}(<\/div>)/g,'<!--  -
 
 body = body.replace(/(<div class="header fixed-top">)[\s\S[\d\D]{0,345}(<\/div>)/g,'<!--  -->');
 
-body = body.replace(/(<div class="banner mt-3">)[\s\S[\d\D]{0,1480}(<\/div>)/g,'<!--  -->');
 
+
+body = body.replace(/(<div class="banner mt-3">)[\s\S[\d\D]{0,2500}(<h3 class="mt-3">今日推荐<\/h3>)/g,'<h3 class="mt-3">今日推荐</h3>');
+
+body = body.replace(/(<div class="banner">)[\s\S[\d\D]{0,2500}(<div class="play mt-3">)/g,'<div class="play mt-3">');
+
+
+
+
+body = body.replace(/(<div class="banner">)[\s\S[\d\D]{0,2500}(<div class="row lists">)/g,'<div class="row lists">');
 
 $done(body);
