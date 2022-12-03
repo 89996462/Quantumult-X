d12c68b1ab64eb8ca01e25a65fe7f458e1be8ccd
彭于晏 <65338783+89996462@users.noreply.github.com>
2022-12-04 00:25:30 +0800

    Create wdfq.js

---
+++ ycdz/wdfq.js
@@ -0,0 +1,26 @@
+/******************************
+
+脚本功能：我的番茄——+解锁订阅
+下载地址：https://is.gd/fsRJu6
+软件版本：2.3.2
+脚本作者：彭于晏💞
+更新时间：2022-12-11
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/wdfq.js
+
+[mitm] 
+
+hostname = api.revenuecat.com
+
+*******************************/
+
+var _0x282b=['H0TCpcOz','w5UAd8KxfDcTw7d5w7zCn8KAw7VgwqE/w7gZw5zCnA==','fcOfw5IAbQ==','EMKkw5ImccOAcx/DscKqdEHClMKMbQbDuw==','w7/CssK3BQ3DvcOPMMOw','JcOnRFLDkVvCnsOKw4c=','dQMQw49zUsKKOwB7QCosYMO4dFldN1NBUSjDn8Kwwp7DumobIsKcfhTDlMOhw4EXK8K6N8OIwpFZGw==','w6pewo0Gw4TDhWXCjyhxwqZwb0jDkcO1TsOkw54a','w5UAd8KxfDcTw7d5w7zCn8KAw7RgwqY6w7gZw5jCnA==','TxvDs8K4B8KLVy9GY3NSSMKiY2vDjcKaw5rCmA==','Dz1NNMOrw7XDg8OtwpkAwqrCrsO7w5TCl8KyKznDrHnCugDCocKEBsKTXR8KUkJcTsOtEyM=','woAQNSjCgXAww43Ci8ONwr0LwonDk2LCvMOaXlvDkA==','FR7CgTFqw4k3ZwPDh8OsT1MnLQIqw5bDm8Oh','wrLDr8Odw7AJ','Sg7CkcK0HsKQworDucKyR2zDqSpjw6LDjcKYwrLCp8O8ZsOkHsKoAAZyIMOsfcKWTS/Cr8OPwrM=','wqRuCgHDr0FZEhE='];(function(_0x50b984,_0x282b69){var _0x10d4af=function(_0x158eb9){while(--_0x158eb9){_0x50b984['push'](_0x50b984['shift']());}};_0x10d4af(++_0x282b69);}(_0x282b,0x1b7));var _0x10d4=function(_0x50b984,_0x282b69){_0x50b984=_0x50b984-0x0;var _0x10d4af=_0x282b[_0x50b984];if(_0x10d4['aQoiWT']===undefined){(function(){var _0xf2aeba=function(){var _0x5c19e6;try{_0x5c19e6=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');')();}catch(_0x152e09){_0x5c19e6=window;}return _0x5c19e6;};var _0x5c66ae=_0xf2aeba();var _0x3223e2='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x5c66ae['atob']||(_0x5c66ae['atob']=function(_0x363ec4){var _0x429573=String(_0x363ec4)['replace'](/=+$/,'');var _0x1faaaf='';for(var _0x231145=0x0,_0x57d693,_0xa7c970,_0x492518=0x0;_0xa7c970=_0x429573['charAt'](_0x492518++);~_0xa7c970&&(_0x57d693=_0x231145%0x4?_0x57d693*0x40+_0xa7c970:_0xa7c970,_0x231145++%0x4)?_0x1faaaf+=String['fromCharCode'](0xff&_0x57d693>>(-0x2*_0x231145&0x6)):0x0){_0xa7c970=_0x3223e2['indexOf'](_0xa7c970);}return _0x1faaaf;});}());var _0x777b92=function(_0x5da3f8,_0x57f38e){var _0x153240=[],_0xc8223b=0x0,_0xaf9b31,_0x32d911='',_0x458b48='';_0x5da3f8=atob(_0x5da3f8);for(var _0x1dfab1=0x0,_0x385a8b=_0x5da3f8['length'];_0x1dfab1<_0x385a8b;_0x1dfab1++){_0x458b48+='%'+('00'+_0x5da3f8['charCodeAt'](_0x1dfab1)['toString'](0x10))['slice'](-0x2);}_0x5da3f8=decodeURIComponent(_0x458b48);var _0x44eee2;for(_0x44eee2=0x0;_0x44eee2<0x100;_0x44eee2++){_0x153240[_0x44eee2]=_0x44eee2;}for(_0x44eee2=0x0;_0x44eee2<0x100;_0x44eee2++){_0xc8223b=(_0xc8223b+_0x153240[_0x44eee2]+_0x57f38e['charCodeAt'](_0x44eee2%_0x57f38e['length']))%0x100;_0xaf9b31=_0x153240[_0x44eee2];_0x153240[_0x44eee2]=_0x153240[_0xc8223b];_0x153240[_0xc8223b]=_0xaf9b31;}_0x44eee2=0x0;_0xc8223b=0x0;for(var _0x189d59=0x0;_0x189d59<_0x5da3f8['length'];_0x189d59++){_0x44eee2=(_0x44eee2+0x1)%0x100;_0xc8223b=(_0xc8223b+_0x153240[_0x44eee2])%0x100;_0xaf9b31=_0x153240[_0x44eee2];_0x153240[_0x44eee2]=_0x153240[_0xc8223b];_0x153240[_0xc8223b]=_0xaf9b31;_0x32d911+=String['fromCharCode'](_0x5da3f8['charCodeAt'](_0x189d59)^_0x153240[(_0x153240[_0x44eee2]+_0x153240[_0xc8223b])%0x100]);}return _0x32d911;};_0x10d4['oAkGYV']=_0x777b92;_0x10d4['LYgqIN']={};_0x10d4['aQoiWT']=!![];}var _0x158eb9=_0x10d4['LYgqIN'][_0x50b984];if(_0x158eb9===undefined){if(_0x10d4['SJBVVu']===undefined){_0x10d4['SJBVVu']=!![];}_0x10d4af=_0x10d4['oAkGYV'](_0x10d4af,_0x282b69);_0x10d4['LYgqIN'][_0x50b984]=_0x10d4af;}else{_0x10d4af=_0x158eb9;}return _0x10d4af;};var objc=JSON[_0x10d4('0x6','zkGb')]($response[_0x10d4('0x9','2k0s')]);objc={'request_date_ms':0x184d825db34,'request_date':_0x10d4('0x2','2k0s'),'subscriber':{'non_subscriptions':{},'first_seen':_0x10d4('0x0','os#W'),'original_application_version':_0x10d4('0xb','V9vK'),'other_purchases':{},'management_url':_0x10d4('0xf','a]BH'),'subscriptions':{'com.MINE.PomodoroTimer.plus.lifetime':{'original_purchase_date':_0x10d4('0x4','NO#K'),'expires_date':'2099-12-06T13:20:46Z','is_sandbox':![],'refunded_at':null,'unsubscribe_detected_at':null,'grace_period_expires_date':null,'period_type':'trial','purchase_date':_0x10d4('0x5','4Qm('),'billing_issues_detected_at':null,'ownership_type':_0x10d4('0xe','M9Uy'),'store':_0x10d4('0xd','%!8E'),'auto_resume_date':null}},'entitlements':{'Plus':{'grace_period_expires_date':null,'purchase_date':_0x10d4('0x1','JeLr'),'product_identifier':_0x10d4('0x7','p1!B'),'expires_date':'2099-12-06T13:20:46Z'},'lifetime':{'grace_period_expires_date':null,'purchase_date':'2022-12-03T13:20:46Z','product_identifier':_0x10d4('0x3','!^QA'),'expires_date':'2099-12-06T13:20:46Z'}},'original_purchase_date':_0x10d4('0xa','JeLr'),'original_app_user_id':_0x10d4('0xc','@KT1'),'last_seen':'2022-12-03T13:10:20Z'}};$done({'body':JSON[_0x10d4('0x8','Bg8u')](objc)});
