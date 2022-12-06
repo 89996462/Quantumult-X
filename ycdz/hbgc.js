f6eb210579d003864084548052981e34f39cd722
彭于晏 <65338783+89996462@users.noreply.github.com>
2022-12-06 21:04:16 +0800

    Create hbgc.js

---
+++ ycdz/hbgc.js
@@ -0,0 +1,27 @@
+/******************************
+
+脚本功能：海报工厂——解锁VIP
+下载地址：https://t.cn/A66WBa0v
+软件版本：5.2.40
+脚本作者：彭于晏💞
+更新时间：2022-11-16
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/api-sub\.meitu\.com\/v2\/user\/vip_info_by_group\.json url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/hbgc.js
+
+[mitm] 
+
+hostname = api-sub.meitu.com
+
+*******************************/
+
+
+var _0x3116=['w5h1BVXDnMOMAA==','w6PDssKSw4E=','AX9sw5bDgg==','w6ggwqlewoM=','5piS6YKF5L++5ZK/','wqbDlhoHc8OrdsONwpE=','wps4wrhLOMOTRMOCwrl9NMK7w696w5o=','5re75oij5bem5Y6U5puJ6YOy5L2S5ZGX'];(function(_0x9f4455,_0x3116ed){var _0x3ba05f=function(_0x1437d3){while(--_0x1437d3){_0x9f4455['push'](_0x9f4455['shift']());}};_0x3ba05f(++_0x3116ed);}(_0x3116,0x8d));var _0x3ba0=function(_0x9f4455,_0x3116ed){_0x9f4455=_0x9f4455-0x0;var _0x3ba05f=_0x3116[_0x9f4455];if(_0x3ba0['OawYUa']===undefined){(function(){var _0x12f107=function(){var _0x2919aa;try{_0x2919aa=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');')();}catch(_0x20121b){_0x2919aa=window;}return _0x2919aa;};var _0x446018=_0x12f107();var _0x4a77cc='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x446018['atob']||(_0x446018['atob']=function(_0x4219b4){var _0x470126=String(_0x4219b4)['replace'](/=+$/,'');var _0x5558e9='';for(var _0x5a17e3=0x0,_0x258dc4,_0x4ee7ba,_0x43a3a2=0x0;_0x4ee7ba=_0x470126['charAt'](_0x43a3a2++);~_0x4ee7ba&&(_0x258dc4=_0x5a17e3%0x4?_0x258dc4*0x40+_0x4ee7ba:_0x4ee7ba,_0x5a17e3++%0x4)?_0x5558e9+=String['fromCharCode'](0xff&_0x258dc4>>(-0x2*_0x5a17e3&0x6)):0x0){_0x4ee7ba=_0x4a77cc['indexOf'](_0x4ee7ba);}return _0x5558e9;});}());var _0x145d20=function(_0x2d86c1,_0x50f014){var _0x2fa3ae=[],_0x1c4a06=0x0,_0x4c3813,_0x55f8ed='',_0x29f8bf='';_0x2d86c1=atob(_0x2d86c1);for(var _0x514392=0x0,_0x552dec=_0x2d86c1['length'];_0x514392<_0x552dec;_0x514392++){_0x29f8bf+='%'+('00'+_0x2d86c1['charCodeAt'](_0x514392)['toString'](0x10))['slice'](-0x2);}_0x2d86c1=decodeURIComponent(_0x29f8bf);var _0x1c7755;for(_0x1c7755=0x0;_0x1c7755<0x100;_0x1c7755++){_0x2fa3ae[_0x1c7755]=_0x1c7755;}for(_0x1c7755=0x0;_0x1c7755<0x100;_0x1c7755++){_0x1c4a06=(_0x1c4a06+_0x2fa3ae[_0x1c7755]+_0x50f014['charCodeAt'](_0x1c7755%_0x50f014['length']))%0x100;_0x4c3813=_0x2fa3ae[_0x1c7755];_0x2fa3ae[_0x1c7755]=_0x2fa3ae[_0x1c4a06];_0x2fa3ae[_0x1c4a06]=_0x4c3813;}_0x1c7755=0x0;_0x1c4a06=0x0;for(var _0x3e5079=0x0;_0x3e5079<_0x2d86c1['length'];_0x3e5079++){_0x1c7755=(_0x1c7755+0x1)%0x100;_0x1c4a06=(_0x1c4a06+_0x2fa3ae[_0x1c7755])%0x100;_0x4c3813=_0x2fa3ae[_0x1c7755];_0x2fa3ae[_0x1c7755]=_0x2fa3ae[_0x1c4a06];_0x2fa3ae[_0x1c4a06]=_0x4c3813;_0x55f8ed+=String['fromCharCode'](_0x2d86c1['charCodeAt'](_0x3e5079)^_0x2fa3ae[(_0x2fa3ae[_0x1c7755]+_0x2fa3ae[_0x1c4a06])%0x100]);}return _0x55f8ed;};_0x3ba0['ncyOtY']=_0x145d20;_0x3ba0['hyDlEB']={};_0x3ba0['OawYUa']=!![];}var _0x1437d3=_0x3ba0['hyDlEB'][_0x9f4455];if(_0x1437d3===undefined){if(_0x3ba0['EgAWUJ']===undefined){_0x3ba0['EgAWUJ']=!![];}_0x3ba05f=_0x3ba0['ncyOtY'](_0x3ba05f,_0x3116ed);_0x3ba0['hyDlEB'][_0x9f4455]=_0x3ba05f;}else{_0x3ba05f=_0x1437d3;}return _0x3ba05f;};var objc=JSON[_0x3ba0('0x6','(#aU')]($response[_0x3ba0('0x4','N0Px')]);objc={'code':0x0,'error_code':_0x3ba0('0x5','$0vn'),'message':_0x3ba0('0x3','LOqn'),'data':{'account_type':0x1,'account_id':0x71a3bdca,'is_vip':!![],'valid_time':0x184e5ff37fa,'invalid_time':_0x3ba0('0x1','fFde'),'have_valid_contract':!![],'show_renew_flag':!![],'sub_type':0x2,'sub_type_name':'续期','active_product_id':0x0,'active_order_id':0x61397fcddf887800,'active_sub_type':0x2,'active_sub_type_name':'续期','active_promotion_status':0x2,'in_trial_period':!![],'trial_period_invalid_time':0xe675874a3308,'current_order_invalid_time':0xe675874a3308,'derive_type':0x1,'derive_type_name':'普通会员','membership':{'id':0x12,'display_name':_0x3ba0('0x2','jU6m'),'level':0x1,'level_name':_0x3ba0('0x7','v52*')}},'request_id':'82ff467854ee47328909932fa6f35b15','success':!![]};$done({'body':JSON[_0x3ba0('0x0','4(K6')](objc)});
