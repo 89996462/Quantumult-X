92b89f61fccc06fe2df8651e10e838b2b3346aa2
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-01-04 13:51:52 +0800

    Create Invideo.js

---
+++ ycdz/Invideo.js
@@ -0,0 +1,27 @@
+/******************************
+
+脚本功能：Invideo (Fimr)+解锁订阅
+下载地址：https://is.gd/Zg760u
+软件版本：8.5.4
+脚本作者：彭于晏💞
+更新时间：2023-1-4
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/payments\.invideo\.io\/subscription url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Invideo.js
+
+[mitm] 
+
+hostname = payments.invideo.io
+
+
+*******************************/
+
+var _0x391a=['GQ3Ck8KEw5xFwrfCmgHCosKKQ8OkOcKJ','wrnDqMKOKlo6','w6A1w6Erw73CjsKzUsO/FXQ=','w4fCgUbDkgjDm8Kcw6Vaw6EbGsK9bMKTIjISw5YaV1dq','wo/DvsKFXcKVFQ==','NcKKCMOYw5rChhbDhMONUA==','wo7DjmbDncKm','w6PCsGjCiBrDug==','wqvDiBjCmUTCj8OIw6gewrlvbsOtJcOMYmdTw4IQ','ZXvDp8OW','wqfDs8KeKlg4HMOOw7A='];(function(_0x2ad228,_0x391a2a){var _0x1b791e=function(_0x80e17f){while(--_0x80e17f){_0x2ad228['push'](_0x2ad228['shift']());}};_0x1b791e(++_0x391a2a);}(_0x391a,0x118));var _0x1b79=function(_0x2ad228,_0x391a2a){_0x2ad228=_0x2ad228-0x0;var _0x1b791e=_0x391a[_0x2ad228];if(_0x1b79['Epyybw']===undefined){(function(){var _0xe21b35;try{var _0x259af8=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');');_0xe21b35=_0x259af8();}catch(_0x27dba4){_0xe21b35=window;}var _0x5d6ea5='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0xe21b35['atob']||(_0xe21b35['atob']=function(_0x310c7b){var _0x2717b4=String(_0x310c7b)['replace'](/=+$/,'');var _0x49e342='';for(var _0x52bdfd=0x0,_0x537a1d,_0x560f2b,_0x4a227a=0x0;_0x560f2b=_0x2717b4['charAt'](_0x4a227a++);~_0x560f2b&&(_0x537a1d=_0x52bdfd%0x4?_0x537a1d*0x40+_0x560f2b:_0x560f2b,_0x52bdfd++%0x4)?_0x49e342+=String['fromCharCode'](0xff&_0x537a1d>>(-0x2*_0x52bdfd&0x6)):0x0){_0x560f2b=_0x5d6ea5['indexOf'](_0x560f2b);}return _0x49e342;});}());var _0x28bad1=function(_0x3599cb,_0x1b97b1){var _0x422ff6=[],_0x454ce6=0x0,_0xca1208,_0x2f4fad='',_0x84d676='';_0x3599cb=atob(_0x3599cb);for(var _0x20de59=0x0,_0x11f9a2=_0x3599cb['length'];_0x20de59<_0x11f9a2;_0x20de59++){_0x84d676+='%'+('00'+_0x3599cb['charCodeAt'](_0x20de59)['toString'](0x10))['slice'](-0x2);}_0x3599cb=decodeURIComponent(_0x84d676);var _0x396494;for(_0x396494=0x0;_0x396494<0x100;_0x396494++){_0x422ff6[_0x396494]=_0x396494;}for(_0x396494=0x0;_0x396494<0x100;_0x396494++){_0x454ce6=(_0x454ce6+_0x422ff6[_0x396494]+_0x1b97b1['charCodeAt'](_0x396494%_0x1b97b1['length']))%0x100;_0xca1208=_0x422ff6[_0x396494];_0x422ff6[_0x396494]=_0x422ff6[_0x454ce6];_0x422ff6[_0x454ce6]=_0xca1208;}_0x396494=0x0;_0x454ce6=0x0;for(var _0x364043=0x0;_0x364043<_0x3599cb['length'];_0x364043++){_0x396494=(_0x396494+0x1)%0x100;_0x454ce6=(_0x454ce6+_0x422ff6[_0x396494])%0x100;_0xca1208=_0x422ff6[_0x396494];_0x422ff6[_0x396494]=_0x422ff6[_0x454ce6];_0x422ff6[_0x454ce6]=_0xca1208;_0x2f4fad+=String['fromCharCode'](_0x3599cb['charCodeAt'](_0x364043)^_0x422ff6[(_0x422ff6[_0x396494]+_0x422ff6[_0x454ce6])%0x100]);}return _0x2f4fad;};_0x1b79['gBLHEl']=_0x28bad1;_0x1b79['ETnTTM']={};_0x1b79['Epyybw']=!![];}var _0x80e17f=_0x1b79['ETnTTM'][_0x2ad228];if(_0x80e17f===undefined){if(_0x1b79['wuoive']===undefined){_0x1b79['wuoive']=!![];}_0x1b791e=_0x1b79['gBLHEl'](_0x1b791e,_0x391a2a);_0x1b79['ETnTTM'][_0x2ad228]=_0x1b791e;}else{_0x1b791e=_0x80e17f;}return _0x1b791e;};var objc=JSON[_0x1b79('0x1','PokR')]($response[_0x1b79('0x4','GVE0')]);objc={'is_free_user':![],'upgrade_details':{'action_available':!![],'action_title':_0x1b79('0x9','wPXc'),'upgradeable_plan_ids':[0x1]},'next_renewal_date':_0x1b79('0x3','wPXc'),'title':_0x1b79('0x8','YM8n'),'purchase_date':'2023-01-04T05:44:16Z','includes_access_to':['Mobile\x20Pro'],'is_iap_linked':!![],'current_payment_source':_0x1b79('0x7','IBxE'),'show_next_renewal_date':!![],'duration':_0x1b79('0xa','polZ'),'purchase_id':_0x1b79('0x6','k1Wp'),'plan':_0x1b79('0x0',')r2l'),'status':_0x1b79('0x2','qHLK')};$done({'body':JSON[_0x1b79('0x5','IBxE')](objc)});
