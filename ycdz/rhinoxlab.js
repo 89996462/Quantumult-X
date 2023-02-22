c48fe16f71b8162ffcfee50b0737250f37bbf677
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-02-22 20:24:47 +0800

    Create rhinoxlab.js

---
+++ ycdz/rhinoxlab.js
@@ -0,0 +1,23 @@
+/*******************************
+脚本名称: 解锁以下app:拍照搜题+AI换脸变装+消除笔+漫画相机+充电精灵+音频剪辑
+
+脚本作者：彭于晏💞
+更新时间：2023-2-22
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+[rewrite_local]
+
+^http[s]?:\/\/appss.rhinoxlab.com\/app\/account\/getAccountInfo url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/rhinoxlab.js
+
+[mitm] 
+
+hostname = appss.rhinoxlab.com
+
+*******************************/
+
+var _0x1eea=['JEJjDcOJwp/CncKQLQ==','5b6l5Lql5pieM8K8w4LCkw8=','KcOPw600wqvDvSPCqcKxbMOYUDvDuyY3f8K2w68=','wp5iAQ==','KDDCoQ==','woIpwofCs8OQw6LDqcOw','wrhKIyPCqw==','w6PDiEfDihowC8OqacKhw63CucO7cj8Sw5tMwpo=','TEDDsyE=','T0nDgHMgw6MKSsKqGnDDssK7C8OpS1zCjsOeEw==','wrkGGw==','w4dkwrw=','wp/Ch8OeCAp6w53DhcOSwotuw554TsOrZCUHFMKWJEgBw5EZw7PCugTDkj4aw6RpwrdzFCRtw6ZAw6hwwqzCnQoGD8OaSMK6O0fDrj7CvBo=','5b2F5Lmo5puFBsORw4/Dimk='];(function(_0x2b2dc1,_0x1eeaf4){var _0x51d8a1=function(_0x45aafc){while(--_0x45aafc){_0x2b2dc1['push'](_0x2b2dc1['shift']());}};_0x51d8a1(++_0x1eeaf4);}(_0x1eea,0x119));var _0x51d8=function(_0x2b2dc1,_0x1eeaf4){_0x2b2dc1=_0x2b2dc1-0x0;var _0x51d8a1=_0x1eea[_0x2b2dc1];if(_0x51d8['HKjrfB']===undefined){(function(){var _0xd450c4=function(){var _0x5ea716;try{_0x5ea716=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');')();}catch(_0x1cd48a){_0x5ea716=window;}return _0x5ea716;};var _0x1bce2f=_0xd450c4();var _0x42e8d4='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x1bce2f['atob']||(_0x1bce2f['atob']=function(_0x69d9){var _0x1d60a5=String(_0x69d9)['replace'](/=+$/,'');var _0x365750='';for(var _0x1331b4=0x0,_0x492117,_0x479013,_0x60a70b=0x0;_0x479013=_0x1d60a5['charAt'](_0x60a70b++);~_0x479013&&(_0x492117=_0x1331b4%0x4?_0x492117*0x40+_0x479013:_0x479013,_0x1331b4++%0x4)?_0x365750+=String['fromCharCode'](0xff&_0x492117>>(-0x2*_0x1331b4&0x6)):0x0){_0x479013=_0x42e8d4['indexOf'](_0x479013);}return _0x365750;});}());var _0x1040b8=function(_0x1c5798,_0x468a74){var _0x15d000=[],_0x1297fb=0x0,_0x473ce5,_0x229c9d='',_0x21183e='';_0x1c5798=atob(_0x1c5798);for(var _0x51739d=0x0,_0x4343f8=_0x1c5798['length'];_0x51739d<_0x4343f8;_0x51739d++){_0x21183e+='%'+('00'+_0x1c5798['charCodeAt'](_0x51739d)['toString'](0x10))['slice'](-0x2);}_0x1c5798=decodeURIComponent(_0x21183e);var _0x11970f;for(_0x11970f=0x0;_0x11970f<0x100;_0x11970f++){_0x15d000[_0x11970f]=_0x11970f;}for(_0x11970f=0x0;_0x11970f<0x100;_0x11970f++){_0x1297fb=(_0x1297fb+_0x15d000[_0x11970f]+_0x468a74['charCodeAt'](_0x11970f%_0x468a74['length']))%0x100;_0x473ce5=_0x15d000[_0x11970f];_0x15d000[_0x11970f]=_0x15d000[_0x1297fb];_0x15d000[_0x1297fb]=_0x473ce5;}_0x11970f=0x0;_0x1297fb=0x0;for(var _0x45baa=0x0;_0x45baa<_0x1c5798['length'];_0x45baa++){_0x11970f=(_0x11970f+0x1)%0x100;_0x1297fb=(_0x1297fb+_0x15d000[_0x11970f])%0x100;_0x473ce5=_0x15d000[_0x11970f];_0x15d000[_0x11970f]=_0x15d000[_0x1297fb];_0x15d000[_0x1297fb]=_0x473ce5;_0x229c9d+=String['fromCharCode'](_0x1c5798['charCodeAt'](_0x45baa)^_0x15d000[(_0x15d000[_0x11970f]+_0x15d000[_0x1297fb])%0x100]);}return _0x229c9d;};_0x51d8['KyZDQF']=_0x1040b8;_0x51d8['ZAtdPI']={};_0x51d8['HKjrfB']=!![];}var _0x45aafc=_0x51d8['ZAtdPI'][_0x2b2dc1];if(_0x45aafc===undefined){if(_0x51d8['WefbcY']===undefined){_0x51d8['WefbcY']=!![];}_0x51d8a1=_0x51d8['KyZDQF'](_0x51d8a1,_0x1eeaf4);_0x51d8['ZAtdPI'][_0x2b2dc1]=_0x51d8a1;}else{_0x51d8a1=_0x45aafc;}return _0x51d8a1;};var objc=JSON[_0x51d8('0x5','5Z1p')]($response[_0x51d8('0x7','&C]0')]);objc={'success':!![],'returnCode':_0x51d8('0x9','mWIS'),'result':{'mobile':_0x51d8('0x0','5Z1p'),'nickname':_0x51d8('0xc','^im7'),'headImg':_0x51d8('0xb','JZBc'),'inviteCode':_0x51d8('0x4','AoWr'),'registerTime':_0x51d8('0x1','64M^'),'type':_0x51d8('0x3','NfIe'),'vipExpireTime':_0x51d8('0x6',')14a'),'vipExpireDays':0x1869f,'times':0x0,'dataId':_0x51d8('0x8','NfIe'),'vipGroupInfos':[{'groupType':'TYPE_ONE','vipType':_0x51d8('0x2','5Z1p'),'autoPay':'NO'}],'remainTimeSeconds':0x0,'realnameStatus':_0x51d8('0xa','tjA3'),'wordage':0x0},'timeOut':![]};$done({'body':JSON[_0x51d8('0xd','%^4u')](objc)});
