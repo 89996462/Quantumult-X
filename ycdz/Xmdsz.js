a1d5a92356f87516f7e483b03747fc7c34a2309f
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-03-02 11:31:58 +0800

    Update Xmdsz.js

--- ycdz/Xmdsz.js
+++ ycdz/Xmdsz.js
@@ -16,7 +16,7 @@ TG频道群：https://t.me/py
 
 [rewrite_local]
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body Xmdsz.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Xmdsz.js
 
 [mitm] 
 
