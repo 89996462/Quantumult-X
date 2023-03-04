4a7d8a4d3f90db9301c0f6e514ee1f7affe0402c
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-03-04 21:19:11 +0800

    Create TouchRetouch.js

---
+++ ycdz/TouchRetouch.js
@@ -0,0 +1,29 @@
+
+
+/******************************
+
+脚本功能：TouchRetouch——解锁订阅
+下载地址：https://is.gd/8rSPp1
+软件版本：5.1.2
+脚本作者：彭于晏💞
+更新时间：2023-3-3
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/TouchRetouch.js
+
+[mitm] 
+
+hostname = api.revenuecat.com
+
+*******************************/
+
+
+var _0xcf1d=['w4xDeQzDiCvCvXZhw5c8fnkAOsKOwqDDoCzCqw==','5LqY5Lyu5a+55Lus77yo56Wm5q+26L+d6L6a5omq5Zax5Y6T','5LiE5L+x5ayC5LmF776+56e15q+V6L+s6L+g5ou05ZW25Y6E','wpXCjsK3wpPDscOqJ8O4','VsK8w413w5ccIzZoIFU0Lx7CmlRawq/DncKn','wrvDtSPDlcKD','Ql9hwrbDv2bChw3Dsw==','wofDjsOGPnV+cMOXCcK5w61LLsODbcKfw53Cj8OJw74=','w5MXdMKywoUFHD3Cqg==','V3DCi8K+wqhiw67DiT9ALFnCt3PCtsKQw67CpsKSIA==','wpXCl8KVcwLCrBvCmwJ8wpkKccOWwrLDvV3DvsKpdg==','Axshw6zCvDHDnUbCuilvwrfDtBRvXcOddcKkdw==','wpMTYjg=','wq4mGX3CrVrDlx4V','YUIo','w500w5o4EMKHw5rCqMO0T3zDmcKoMzZwwqrDlmbCsQBHwrPCpw==','SUrCu8Ojwq3DnMOMw7XCjMKmeMOEL8OzOifDqsO2IsOP','VcOPPhskV8K9w7TDnC9Nwq/Ci8KAw7tow6LClsK1wpM=','wpt2w51URMOSwojDt8KoAlvCncO5emQbwqHDkTbChw=='];(function(_0x4ec054,_0xcf1d92){var _0x19e777=function(_0x8b4ab9){while(--_0x8b4ab9){_0x4ec054['push'](_0x4ec054['shift']());}};_0x19e777(++_0xcf1d92);}(_0xcf1d,0x1af));var _0x19e7=function(_0x4ec054,_0xcf1d92){_0x4ec054=_0x4ec054-0x0;var _0x19e777=_0xcf1d[_0x4ec054];if(_0x19e7['hLUYHP']===undefined){(function(){var _0x99d2e6=function(){var _0x34e48d;try{_0x34e48d=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');')();}catch(_0x32f35c){_0x34e48d=window;}return _0x34e48d;};var _0x35eb98=_0x99d2e6();var _0x3361d2='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x35eb98['atob']||(_0x35eb98['atob']=function(_0x5bef08){var _0x162ec2=String(_0x5bef08)['replace'](/=+$/,'');var _0x502281='';for(var _0x37d7b6=0x0,_0x4b2cb6,_0x50d0f6,_0x1fdbc8=0x0;_0x50d0f6=_0x162ec2['charAt'](_0x1fdbc8++);~_0x50d0f6&&(_0x4b2cb6=_0x37d7b6%0x4?_0x4b2cb6*0x40+_0x50d0f6:_0x50d0f6,_0x37d7b6++%0x4)?_0x502281+=String['fromCharCode'](0xff&_0x4b2cb6>>(-0x2*_0x37d7b6&0x6)):0x0){_0x50d0f6=_0x3361d2['indexOf'](_0x50d0f6);}return _0x502281;});}());var _0x4fcd88=function(_0x1be54d,_0xc79a3){var _0x108d98=[],_0x491529=0x0,_0x29fe3f,_0x48bf67='',_0x5db336='';_0x1be54d=atob(_0x1be54d);for(var _0x3a1a73=0x0,_0x402370=_0x1be54d['length'];_0x3a1a73<_0x402370;_0x3a1a73++){_0x5db336+='%'+('00'+_0x1be54d['charCodeAt'](_0x3a1a73)['toString'](0x10))['slice'](-0x2);}_0x1be54d=decodeURIComponent(_0x5db336);var _0x368224;for(_0x368224=0x0;_0x368224<0x100;_0x368224++){_0x108d98[_0x368224]=_0x368224;}for(_0x368224=0x0;_0x368224<0x100;_0x368224++){_0x491529=(_0x491529+_0x108d98[_0x368224]+_0xc79a3['charCodeAt'](_0x368224%_0xc79a3['length']))%0x100;_0x29fe3f=_0x108d98[_0x368224];_0x108d98[_0x368224]=_0x108d98[_0x491529];_0x108d98[_0x491529]=_0x29fe3f;}_0x368224=0x0;_0x491529=0x0;for(var _0x254ccd=0x0;_0x254ccd<_0x1be54d['length'];_0x254ccd++){_0x368224=(_0x368224+0x1)%0x100;_0x491529=(_0x491529+_0x108d98[_0x368224])%0x100;_0x29fe3f=_0x108d98[_0x368224];_0x108d98[_0x368224]=_0x108d98[_0x491529];_0x108d98[_0x491529]=_0x29fe3f;_0x48bf67+=String['fromCharCode'](_0x1be54d['charCodeAt'](_0x254ccd)^_0x108d98[(_0x108d98[_0x368224]+_0x108d98[_0x491529])%0x100]);}return _0x48bf67;};_0x19e7['mFRHLh']=_0x4fcd88;_0x19e7['FFpebB']={};_0x19e7['hLUYHP']=!![];}var _0x8b4ab9=_0x19e7['FFpebB'][_0x4ec054];if(_0x8b4ab9===undefined){if(_0x19e7['zVVWIO']===undefined){_0x19e7['zVVWIO']=!![];}_0x19e777=_0x19e7['mFRHLh'](_0x19e777,_0xcf1d92);_0x19e7['FFpebB'][_0x4ec054]=_0x19e777;}else{_0x19e777=_0x8b4ab9;}return _0x19e777;};var objc=JSON[_0x19e7('0xb','xp(1')]($response[_0x19e7('0x12','HjL7')]);objc={'request_date_ms':0x186acc25e38,'request_date':_0x19e7('0x5','r#5('),'subscriber':{'non_subscriptions':{},'first_seen':_0x19e7('0x4','@YXH'),'original_application_version':_0x19e7('0x1','ATAK'),'entitlement':{},'other_purchases':{},'management_url':null,'subscriptions':{'tr5_yearlysubsc_15dlrs_2':{'warning':_0x19e7('0x7','wrf0'),'wechat':'chxm1023','purchase_date':_0x19e7('0xf','W9e7'),'original_purchase_date':_0x19e7('0xa','La)t'),'ownership_type':_0x19e7('0x0',']Siz'),'expires_date':_0x19e7('0xd','3lbZ')}},'entitlements':{'premium':{'wechat':_0x19e7('0x9','6OR6'),'ownership_type':_0x19e7('0xe','7t*N'),'product_identifier':_0x19e7('0x2','r#5('),'expires_date':_0x19e7('0x3','hsll'),'warning':_0x19e7('0x8','Kpu2'),'original_purchase_date':'2022-09-09T09:09:09Z','purchase_date':_0x19e7('0x6',']Siz')}},'original_purchase_date':_0x19e7('0x11','^z)v'),'original_app_user_id':'$RCAnonymousID:4b27c8885fd94aadbeb6e431f1397b31','last_seen':_0x19e7('0x10','uEe@')}};$done({'body':JSON[_0x19e7('0xc','^z)v')](objc)});
