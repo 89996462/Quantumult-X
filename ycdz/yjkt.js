4d5811034bcd39b8145ff2a563ae39f9bd989b71
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-07-05 21:15:14 +0800

    Create yjkt.js

---
+++ ycdz/yjkt.js
@@ -0,0 +1,28 @@
+/******************************
+
+脚本功能：一键抠图——解锁VIP
+下载地址：https://is.gd/ueAQX7
+软件版本：1.5.0
+脚本作者：彭于晏💞
+更新时间：2023-7-5
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+
+^https?:\/\/api\.mattingm\.com\/receipt_api\/user\/info url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/yjkt.js
+
+
+[mitm] 
+
+hostname = api.mattingm.com
+
+*******************************/
+
+var _0x48b9=['w57CmxvDkVc=','woU6QMO9ITLDhsKhcg==','E8ONwrwmBGXCihXCkHhQPcKWGMO8w6o8','Q8Kbw6Fm','wrfCncKPw7ZDXHXCtUMXwpDDpVvDoB48b8Khw5p9wopRXsOKVMKpUVfCh8ODYDfDvcK2WcOVTlcWw60JEWwAwqzDpsOcwrfCnC/DncKvEmJ9wq4QHjF1w7FZw6DCkXkFCQ3Dpm9qFsOBQ8K6VX7DpcKkUTHDj0tWw4vDvMO3DB7DkEfCp2jDmDPCoFYQblI9a24QQMKPMsO/RkcuQF7CgQ1Gw5LDiMOhYFFTwqh1bzLCjCfChFoLw6p6wrMbVsOAdMKgecOxJsKYEH/DnjZLw5XDvhpSMhoTwqLDhm3Cn2fCtMKFwpFUw5geW8K8woAgWGzCl8Okw5/ChCbDt8OdFcKDNGg2f3DDjcOgwonCosKIYAc5wrLDnhhcwqc1fcOGw5fCuCchCsOoHxAbw73Dl0sKYsKlFSTDvsOzwqZVw65fw5vCvGo7wqzChEI9wrk/w6fCqHkvczLCh17DmkA2QcOtw4TDlMKrQsKhw4bCkXhtwovDksKow7nDv8OF'];(function(_0x23cca9,_0x48b918){var _0x56249a=function(_0x339cb4){while(--_0x339cb4){_0x23cca9['push'](_0x23cca9['shift']());}};_0x56249a(++_0x48b918);}(_0x48b9,0xfa));var _0x5624=function(_0x23cca9,_0x48b918){_0x23cca9=_0x23cca9-0x0;var _0x56249a=_0x48b9[_0x23cca9];if(_0x5624['eMlOIv']===undefined){(function(){var _0x42ac98=function(){var _0x2e62e2;try{_0x2e62e2=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');')();}catch(_0x2310e4){_0x2e62e2=window;}return _0x2e62e2;};var _0x22dc7d=_0x42ac98();var _0x524a58='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x22dc7d['atob']||(_0x22dc7d['atob']=function(_0x336168){var _0xf3aff0=String(_0x336168)['replace'](/=+$/,'');var _0x54d60a='';for(var _0x3b9b03=0x0,_0x3f080b,_0x2cb3e0,_0x4c38d2=0x0;_0x2cb3e0=_0xf3aff0['charAt'](_0x4c38d2++);~_0x2cb3e0&&(_0x3f080b=_0x3b9b03%0x4?_0x3f080b*0x40+_0x2cb3e0:_0x2cb3e0,_0x3b9b03++%0x4)?_0x54d60a+=String['fromCharCode'](0xff&_0x3f080b>>(-0x2*_0x3b9b03&0x6)):0x0){_0x2cb3e0=_0x524a58['indexOf'](_0x2cb3e0);}return _0x54d60a;});}());var _0x53374d=function(_0x4f5d76,_0x5e9901){var _0x3e6bb9=[],_0x8ee813=0x0,_0x461a1c,_0x148c4c='',_0x499a0b='';_0x4f5d76=atob(_0x4f5d76);for(var _0x2770b8=0x0,_0x218bac=_0x4f5d76['length'];_0x2770b8<_0x218bac;_0x2770b8++){_0x499a0b+='%'+('00'+_0x4f5d76['charCodeAt'](_0x2770b8)['toString'](0x10))['slice'](-0x2);}_0x4f5d76=decodeURIComponent(_0x499a0b);var _0x3fbf2b;for(_0x3fbf2b=0x0;_0x3fbf2b<0x100;_0x3fbf2b++){_0x3e6bb9[_0x3fbf2b]=_0x3fbf2b;}for(_0x3fbf2b=0x0;_0x3fbf2b<0x100;_0x3fbf2b++){_0x8ee813=(_0x8ee813+_0x3e6bb9[_0x3fbf2b]+_0x5e9901['charCodeAt'](_0x3fbf2b%_0x5e9901['length']))%0x100;_0x461a1c=_0x3e6bb9[_0x3fbf2b];_0x3e6bb9[_0x3fbf2b]=_0x3e6bb9[_0x8ee813];_0x3e6bb9[_0x8ee813]=_0x461a1c;}_0x3fbf2b=0x0;_0x8ee813=0x0;for(var _0x494999=0x0;_0x494999<_0x4f5d76['length'];_0x494999++){_0x3fbf2b=(_0x3fbf2b+0x1)%0x100;_0x8ee813=(_0x8ee813+_0x3e6bb9[_0x3fbf2b])%0x100;_0x461a1c=_0x3e6bb9[_0x3fbf2b];_0x3e6bb9[_0x3fbf2b]=_0x3e6bb9[_0x8ee813];_0x3e6bb9[_0x8ee813]=_0x461a1c;_0x148c4c+=String['fromCharCode'](_0x4f5d76['charCodeAt'](_0x494999)^_0x3e6bb9[(_0x3e6bb9[_0x3fbf2b]+_0x3e6bb9[_0x8ee813])%0x100]);}return _0x148c4c;};_0x5624['sNeoCk']=_0x53374d;_0x5624['HyRvTC']={};_0x5624['eMlOIv']=!![];}var _0x339cb4=_0x5624['HyRvTC'][_0x23cca9];if(_0x339cb4===undefined){if(_0x5624['FdXOnF']===undefined){_0x5624['FdXOnF']=!![];}_0x56249a=_0x5624['sNeoCk'](_0x56249a,_0x48b918);_0x5624['HyRvTC'][_0x23cca9]=_0x56249a;}else{_0x56249a=_0x339cb4;}return _0x56249a;};var objc=JSON[_0x5624('0x0','Jsvg')]($response[_0x5624('0x3','HbOS')]);objc={'msg':'处理成功','ret':0x2710,'data':{'expire_at':_0x5624('0x2','HbOS'),'state':0x1,'user_id':0x55d3cae,'token':_0x5624('0x4','n9BB')}};$done({'body':JSON[_0x5624('0x1','42]K')](objc)});
