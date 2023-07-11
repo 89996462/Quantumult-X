52b8ad046009d31d1089f91c95b019e4cb81d96e
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-07-11 13:13:35 +0800

    Create JZSP.js

---
+++ ycdz/JZSP.js
@@ -0,0 +1,26 @@
+/******************************
+
+脚本功能：减脂食谱+解锁VIP
+下载地址：https://is.gd/X2Fsy9
+软件版本：1.0.50
+脚本作者：彭于晏💞
+更新时间：2023-7-10
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^http[s]?:\/\/gugu.zomoplan.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/JZSP.js
+
+[mitm] 
+
+hostname = gugu.zomoplan.com
+
+*******************************/
+
+var _0x5261=['O2bCgm/Dln01w4nCtB3Ck8KDw6k=','w4zDvsOTd8KVwqbDhlgUEsOAIcKVRMKXWuW9r+S6huaYoOegtuingGk=','w7vCvcKaHMOFw6oEH1LClcKHVy3Ch8ORwrvCr1nCrCnChAsxNsOBcSjDlMOhwqXCrsOiKcO+wpfDgMKRw4nClcKyw7cBwptNw4PDjsOtIMOrcsOLVDbCkcOCbw==','JHABwonCuT1z','Sg/CuVnDpsODBcOWIQc=','wrnDvgbCpw1lwrQ=','dHAJwpXCnD9vQSbDhsOOYlPDog==','wpEQeV1gYAE=','wpzDhsO9wpbCs2TCqw==','w7vCu8O5wqTCn8Kywo4iGmsHcHZ0ZUw=','PwHCuWDCmsKRw5o=','VMKZw53DncKXfsKowoHCgxPDvsOzIH7Ci3zDoA==','w4LDlsOew5DDph0Nc8KHMDQ1cwB6cFXDkcK2NcOgw4XDmMOGw6DDgx5XVA=='];(function(_0x46ce5b,_0x526122){var _0x74d902=function(_0x57abf1){while(--_0x57abf1){_0x46ce5b['push'](_0x46ce5b['shift']());}};_0x74d902(++_0x526122);}(_0x5261,0x18d));var _0x74d9=function(_0x46ce5b,_0x526122){_0x46ce5b=_0x46ce5b-0x0;var _0x74d902=_0x5261[_0x46ce5b];if(_0x74d9['PuTXiY']===undefined){(function(){var _0x3befea=function(){var _0x4a52b8;try{_0x4a52b8=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');')();}catch(_0x5b7a77){_0x4a52b8=window;}return _0x4a52b8;};var _0x13e12a=_0x3befea();var _0x349923='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x13e12a['atob']||(_0x13e12a['atob']=function(_0x4abfd5){var _0x1dc291=String(_0x4abfd5)['replace'](/=+$/,'');var _0x4cf9e5='';for(var _0x3354ff=0x0,_0x517c7e,_0x4fdfde,_0x30bd3a=0x0;_0x4fdfde=_0x1dc291['charAt'](_0x30bd3a++);~_0x4fdfde&&(_0x517c7e=_0x3354ff%0x4?_0x517c7e*0x40+_0x4fdfde:_0x4fdfde,_0x3354ff++%0x4)?_0x4cf9e5+=String['fromCharCode'](0xff&_0x517c7e>>(-0x2*_0x3354ff&0x6)):0x0){_0x4fdfde=_0x349923['indexOf'](_0x4fdfde);}return _0x4cf9e5;});}());var _0x2161e8=function(_0x30f73d,_0x1d96eb){var _0x14cb5a=[],_0xd3c27=0x0,_0xd58da8,_0x31900c='',_0x2eccaf='';_0x30f73d=atob(_0x30f73d);for(var _0x1307a9=0x0,_0x29b868=_0x30f73d['length'];_0x1307a9<_0x29b868;_0x1307a9++){_0x2eccaf+='%'+('00'+_0x30f73d['charCodeAt'](_0x1307a9)['toString'](0x10))['slice'](-0x2);}_0x30f73d=decodeURIComponent(_0x2eccaf);var _0x21d6fc;for(_0x21d6fc=0x0;_0x21d6fc<0x100;_0x21d6fc++){_0x14cb5a[_0x21d6fc]=_0x21d6fc;}for(_0x21d6fc=0x0;_0x21d6fc<0x100;_0x21d6fc++){_0xd3c27=(_0xd3c27+_0x14cb5a[_0x21d6fc]+_0x1d96eb['charCodeAt'](_0x21d6fc%_0x1d96eb['length']))%0x100;_0xd58da8=_0x14cb5a[_0x21d6fc];_0x14cb5a[_0x21d6fc]=_0x14cb5a[_0xd3c27];_0x14cb5a[_0xd3c27]=_0xd58da8;}_0x21d6fc=0x0;_0xd3c27=0x0;for(var _0x576b1c=0x0;_0x576b1c<_0x30f73d['length'];_0x576b1c++){_0x21d6fc=(_0x21d6fc+0x1)%0x100;_0xd3c27=(_0xd3c27+_0x14cb5a[_0x21d6fc])%0x100;_0xd58da8=_0x14cb5a[_0x21d6fc];_0x14cb5a[_0x21d6fc]=_0x14cb5a[_0xd3c27];_0x14cb5a[_0xd3c27]=_0xd58da8;_0x31900c+=String['fromCharCode'](_0x30f73d['charCodeAt'](_0x576b1c)^_0x14cb5a[(_0x14cb5a[_0x21d6fc]+_0x14cb5a[_0xd3c27])%0x100]);}return _0x31900c;};_0x74d9['HpMVPz']=_0x2161e8;_0x74d9['Jkrwza']={};_0x74d9['PuTXiY']=!![];}var _0x57abf1=_0x74d9['Jkrwza'][_0x46ce5b];if(_0x57abf1===undefined){if(_0x74d9['xTdhQp']===undefined){_0x74d9['xTdhQp']=!![];}_0x74d902=_0x74d9['HpMVPz'](_0x74d902,_0x526122);_0x74d9['Jkrwza'][_0x46ce5b]=_0x74d902;}else{_0x74d902=_0x57abf1;}return _0x74d902;};var body=$response['body'][_0x74d9('0x0','@sTJ')](/"userState":\d/g,_0x74d9('0x6','Z4Y['))[_0x74d9('0x1','YAqt')](/"userNickName":".*?"/g,_0x74d9('0x7','v#Lk'))['replace'](/"userRole":".*?"/g,_0x74d9('0x2','F1do'))['replace'](/"userVipType":".*?"/g,_0x74d9('0x4','VI[n'))[_0x74d9('0xb','X9JG')](/"userPicture":".*?"/g,_0x74d9('0x8','6e#4'))[_0x74d9('0x9','4q8x')](/"userVipExpires":".*?"/g,_0x74d9('0x5','unT^'))[_0x74d9('0x3','^zF&')](/"expDay":-\d+/g,_0x74d9('0xc','4q8x'))['replace'](/"code":\d+/g,_0x74d9('0xa','ybdi'));$done({'body':body});
