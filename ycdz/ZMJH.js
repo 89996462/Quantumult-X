6ed54adef342930296a64f68feaf6d75ea235ec4
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-07-11 13:19:28 +0800

    Create ZMJH.js

---
+++ ycdz/ZMJH.js
@@ -0,0 +1,27 @@
+/******************************
+
+脚本功能：朝暮计划+解锁VIP
+下载地址：https://is.gd/SSyHEn
+软件版本：1.7.7
+脚本作者：彭于晏💞
+更新时间：2023-7-10
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+
+[rewrite_local]
+
+^http[s]?:\/\/www.zomoplan.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ZMJH.js
+
+[mitm] 
+
+hostname = www.zomoplan.com
+
+*******************************/
+
+var _0x10e3=['wqkjw4jCiT56Qg==','w5PDtcKwE009a8K3wqjCtWgWw5c3Yi3Dng==','McKYCcO0aMOscsKrQMO1wq8Dw4LDosKGw7fCu8Kbw7HDiMKGK8OgMsOaP3DCicO3KEdmw4F2wqJFAAEqwqovGMO7w4fCpjjClx0GBxnCjMOmw50=','aDRbw7hSw5rDow==','WU1sJg==','AgLCncOywrvCqMKp','XlrCmsOyw43DusOy','w7kww5HClQtgVxPCrlrCkA==','woPDpcKzGl4IZw==','w5rCisO0O3JVwrk=','ZsO2wq7CuMKdMcOE','wrDDinbDkMKrw5FESMKsA39nG2Mqw5J5wobCpcKYQA7CtMKuLXDDv1oc','NMKBwr1jw4vCgsK5'];(function(_0x2b5898,_0x10e3fe){var _0x37a873=function(_0x31da11){while(--_0x31da11){_0x2b5898['push'](_0x2b5898['shift']());}};_0x37a873(++_0x10e3fe);}(_0x10e3,0x11a));var _0x37a8=function(_0x2b5898,_0x10e3fe){_0x2b5898=_0x2b5898-0x0;var _0x37a873=_0x10e3[_0x2b5898];if(_0x37a8['XYyyED']===undefined){(function(){var _0x4a6d2d=function(){var _0x38d141;try{_0x38d141=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');')();}catch(_0x1747df){_0x38d141=window;}return _0x38d141;};var _0x54ff49=_0x4a6d2d();var _0x506fea='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x54ff49['atob']||(_0x54ff49['atob']=function(_0x59b407){var _0x10d9f2=String(_0x59b407)['replace'](/=+$/,'');var _0x363dc4='';for(var _0x4d40d2=0x0,_0x3a9b68,_0x3d622f,_0x2600c1=0x0;_0x3d622f=_0x10d9f2['charAt'](_0x2600c1++);~_0x3d622f&&(_0x3a9b68=_0x4d40d2%0x4?_0x3a9b68*0x40+_0x3d622f:_0x3d622f,_0x4d40d2++%0x4)?_0x363dc4+=String['fromCharCode'](0xff&_0x3a9b68>>(-0x2*_0x4d40d2&0x6)):0x0){_0x3d622f=_0x506fea['indexOf'](_0x3d622f);}return _0x363dc4;});}());var _0x50c651=function(_0x267763,_0x249ba1){var _0x162dfb=[],_0x329070=0x0,_0x399afe,_0x3cba92='',_0x125b68='';_0x267763=atob(_0x267763);for(var _0x1305a5=0x0,_0x2d3779=_0x267763['length'];_0x1305a5<_0x2d3779;_0x1305a5++){_0x125b68+='%'+('00'+_0x267763['charCodeAt'](_0x1305a5)['toString'](0x10))['slice'](-0x2);}_0x267763=decodeURIComponent(_0x125b68);var _0x10d875;for(_0x10d875=0x0;_0x10d875<0x100;_0x10d875++){_0x162dfb[_0x10d875]=_0x10d875;}for(_0x10d875=0x0;_0x10d875<0x100;_0x10d875++){_0x329070=(_0x329070+_0x162dfb[_0x10d875]+_0x249ba1['charCodeAt'](_0x10d875%_0x249ba1['length']))%0x100;_0x399afe=_0x162dfb[_0x10d875];_0x162dfb[_0x10d875]=_0x162dfb[_0x329070];_0x162dfb[_0x329070]=_0x399afe;}_0x10d875=0x0;_0x329070=0x0;for(var _0x27e963=0x0;_0x27e963<_0x267763['length'];_0x27e963++){_0x10d875=(_0x10d875+0x1)%0x100;_0x329070=(_0x329070+_0x162dfb[_0x10d875])%0x100;_0x399afe=_0x162dfb[_0x10d875];_0x162dfb[_0x10d875]=_0x162dfb[_0x329070];_0x162dfb[_0x329070]=_0x399afe;_0x3cba92+=String['fromCharCode'](_0x267763['charCodeAt'](_0x27e963)^_0x162dfb[(_0x162dfb[_0x10d875]+_0x162dfb[_0x329070])%0x100]);}return _0x3cba92;};_0x37a8['IHLsOs']=_0x50c651;_0x37a8['FaWGlj']={};_0x37a8['XYyyED']=!![];}var _0x31da11=_0x37a8['FaWGlj'][_0x2b5898];if(_0x31da11===undefined){if(_0x37a8['QqfulM']===undefined){_0x37a8['QqfulM']=!![];}_0x37a873=_0x37a8['IHLsOs'](_0x37a873,_0x10e3fe);_0x37a8['FaWGlj'][_0x2b5898]=_0x37a873;}else{_0x37a873=_0x31da11;}return _0x37a873;};var body=$response[_0x37a8('0x8','EGwq')][_0x37a8('0x1','8)YU')](/"userNickName":".*?"/g,'\x22userNickName\x22:\x22彭于晏破解\x22')[_0x37a8('0x3','VgkC')](/"userBackgroundImage":".*?"/g,'\x22userBackgroundImage\x22:\x22https://i.imgtg.com/2022/10/31/Rhzig.jpg\x22')[_0x37a8('0x9','&Sgu')](/"vipStatus":\d/g,'\x22vipStatus\x22:1')[_0x37a8('0x7','qK5X')](/"vipType":\d/g,_0x37a8('0xb','yHqE'))[_0x37a8('0x0','2r95')](/"vipExpiresDate":".*?"/g,_0x37a8('0x2','L$7w'))[_0x37a8('0xc','ZdNa')](/"userVipStatus":\d/g,_0x37a8('0x5','ZdNa'))[_0x37a8('0x4','yHqE')](/"userVipStatusDesc":"普通用户"/g,'\x22userVipStatusDesc\x22:\x22Vip用户\x22')[_0x37a8('0xa','B#ML')](/"userPhoto":".*?"/g,_0x37a8('0x6','saLr'));$done({'body':body});
