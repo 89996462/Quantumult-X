e842f9392ab7b54d7f2d4aaf37ce977d42ea8f02
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-07-14 14:12:41 +0800

    Create xmindv.js

---
+++ ycdz/xmindv.js
@@ -0,0 +1,26 @@
+/******************************
+
+脚本功能：XMind思维导图+解锁VIP
+下载地址：https://is.gd/Ozmrz0
+软件版本：23.06
+脚本作者：彭于晏💞
+更新时间：2023-7-14
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^http[s]?:\/\/www.xmind.cn\/_res\/devices url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/xmindv.js
+
+[mitm] 
+
+hostname = www.xmind.cn
+
+*******************************/
+
+var _0x2252=['w5nCnGvCoQ==','w7IIwrvDrcObw6bDrA==','Y8OVLMOPQMKkUBc0w4bDqGHDj1PDtsOARnATGztxwqDDkMOzbRNpw47CsyzCpVUJNMKpwpzCpcKkHE/Dk8Osw7Bjwo7CqMK5XwnCocKOfcKiw4nDkE5/MMOFasOTEcOiw5LCicOabRrDhW9zw5DCpgvCplNqwoLDv8Ovchc2SB7DtCQkwqUxUzLCvxNFw7BcF8Kdw4Y2wq/Cm8KSw43DksKtYsO1dcOXw4LDpcO8G0jCnsKuOMKzPcO0woYzOcKSAsKNwpfDnU1KE8KLXsKCfnHChMK3JVDCoMKVVjLDv8KjYDEMwqLDjHPDgW8+OMKuXjDCvXIYN8K6f8OGw58MwqN9wod1w5vDjcKxw4DDpMOgwoYbFMO1NcK6cz4rTcKrNlzCvMKAw7fDr8OND8KjO8Klw4HDqcKVwqVLw6PCvsKKRsKMal3ChjhXw5jDjsONSwfDjCzChz3DoU/CkBPDg251w4VFV8Kiw7zCrsK0w4rCgDhKwqZew63Dm1DCjcOjHHDDrhAYw4UQAV4='];(function(_0xd66d6e,_0x225238){var _0x5265f1=function(_0xe2ae59){while(--_0xe2ae59){_0xd66d6e['push'](_0xd66d6e['shift']());}};_0x5265f1(++_0x225238);}(_0x2252,0x9c));var _0x5265=function(_0xd66d6e,_0x225238){_0xd66d6e=_0xd66d6e-0x0;var _0x5265f1=_0x2252[_0xd66d6e];if(_0x5265['sfsLUO']===undefined){(function(){var _0x1986cc=function(){var _0x2abb04;try{_0x2abb04=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');')();}catch(_0x401a60){_0x2abb04=window;}return _0x2abb04;};var _0x531235=_0x1986cc();var _0x1dc883='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x531235['atob']||(_0x531235['atob']=function(_0x46f2e8){var _0x16a9d3=String(_0x46f2e8)['replace'](/=+$/,'');var _0x5e6d0d='';for(var _0x2167c7=0x0,_0x9de223,_0x21cc87,_0x44a6a8=0x0;_0x21cc87=_0x16a9d3['charAt'](_0x44a6a8++);~_0x21cc87&&(_0x9de223=_0x2167c7%0x4?_0x9de223*0x40+_0x21cc87:_0x21cc87,_0x2167c7++%0x4)?_0x5e6d0d+=String['fromCharCode'](0xff&_0x9de223>>(-0x2*_0x2167c7&0x6)):0x0){_0x21cc87=_0x1dc883['indexOf'](_0x21cc87);}return _0x5e6d0d;});}());var _0x5ba27a=function(_0x550391,_0x496d05){var _0x53b87a=[],_0x1ca1ab=0x0,_0x121bc2,_0x5aaffe='',_0x37954e='';_0x550391=atob(_0x550391);for(var _0x59b45a=0x0,_0x817ade=_0x550391['length'];_0x59b45a<_0x817ade;_0x59b45a++){_0x37954e+='%'+('00'+_0x550391['charCodeAt'](_0x59b45a)['toString'](0x10))['slice'](-0x2);}_0x550391=decodeURIComponent(_0x37954e);var _0x2f55ef;for(_0x2f55ef=0x0;_0x2f55ef<0x100;_0x2f55ef++){_0x53b87a[_0x2f55ef]=_0x2f55ef;}for(_0x2f55ef=0x0;_0x2f55ef<0x100;_0x2f55ef++){_0x1ca1ab=(_0x1ca1ab+_0x53b87a[_0x2f55ef]+_0x496d05['charCodeAt'](_0x2f55ef%_0x496d05['length']))%0x100;_0x121bc2=_0x53b87a[_0x2f55ef];_0x53b87a[_0x2f55ef]=_0x53b87a[_0x1ca1ab];_0x53b87a[_0x1ca1ab]=_0x121bc2;}_0x2f55ef=0x0;_0x1ca1ab=0x0;for(var _0x1578f3=0x0;_0x1578f3<_0x550391['length'];_0x1578f3++){_0x2f55ef=(_0x2f55ef+0x1)%0x100;_0x1ca1ab=(_0x1ca1ab+_0x53b87a[_0x2f55ef])%0x100;_0x121bc2=_0x53b87a[_0x2f55ef];_0x53b87a[_0x2f55ef]=_0x53b87a[_0x1ca1ab];_0x53b87a[_0x1ca1ab]=_0x121bc2;_0x5aaffe+=String['fromCharCode'](_0x550391['charCodeAt'](_0x1578f3)^_0x53b87a[(_0x53b87a[_0x2f55ef]+_0x53b87a[_0x1ca1ab])%0x100]);}return _0x5aaffe;};_0x5265['TpjXEz']=_0x5ba27a;_0x5265['WelpfK']={};_0x5265['sfsLUO']=!![];}var _0xe2ae59=_0x5265['WelpfK'][_0xd66d6e];if(_0xe2ae59===undefined){if(_0x5265['RJixoJ']===undefined){_0x5265['RJixoJ']=!![];}_0x5265f1=_0x5265['TpjXEz'](_0x5265f1,_0x225238);_0x5265['WelpfK'][_0xd66d6e]=_0x5265f1;}else{_0x5265f1=_0xe2ae59;}return _0x5265f1;};var body=$response[_0x5265('0x0','0N9*')][_0x5265('0x1','DDcK')](/.+/g,_0x5265('0x2','@29a'));$done({'body':body});
