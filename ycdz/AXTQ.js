1737ebc89c99d55911faaac3d225f541db828731
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-07-18 19:15:20 +0800

    Create AXTQ.js

---
+++ ycdz/AXTQ.js
@@ -0,0 +1,26 @@
+/******************************
+
+脚本功能：安心天气——解锁订阅
+下载地址：https://is.gd/ZqqkR3
+软件版本：1.1.1
+脚本作者：彭于晏💞
+更新时间：2023-7-10
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/AXTQ.js
+
+[MITM]
+
+hostname = buy.itunes.apple.com
+
+********************************/
+
+var _0x10f3=['w6HDrwwCKSoQbsOzwrodcMK5w6Zt','w4rDhcOewqszSMO+PcKeAcKqJ8Om','w4JVdMKDD2x6KMKdw70RBGrDr8OD','w4peARXCosOfTQQtYARVw74=','WsKnR8Khwq7CmTfDmDbCmw==','w4nDg8OfwqotQMO+KcKZA8K6J8OgZnLCiSfDiMOywqHDpsKWwrbDoyPDsRw=','IBLChsKrwrULwodcPcKmBC0VUFRVw4LCicO/wqPCrsKBZhjDtwnCuw5q','wosJS17DtA==','JTIOAg==','eX4ywrjDs3gpAyV7CcO+woXCtFocQ8O0woTDuG7DqsOYw457aW14w7TCui0jw7c/w5RRwq/CmR8='];(function(_0x188f1e,_0x10f383){var _0x5446b8=function(_0x1906ae){while(--_0x1906ae){_0x188f1e['push'](_0x188f1e['shift']());}};_0x5446b8(++_0x10f383);}(_0x10f3,0xcb));var _0x5446=function(_0x188f1e,_0x10f383){_0x188f1e=_0x188f1e-0x0;var _0x5446b8=_0x10f3[_0x188f1e];if(_0x5446['mcXREq']===undefined){(function(){var _0x1d80a2;try{var _0x12bc8e=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');');_0x1d80a2=_0x12bc8e();}catch(_0x5814fe){_0x1d80a2=window;}var _0x12cbc5='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x1d80a2['atob']||(_0x1d80a2['atob']=function(_0x24286e){var _0x29d962=String(_0x24286e)['replace'](/=+$/,'');var _0x357c83='';for(var _0x314931=0x0,_0xd33c8b,_0x373cb9,_0x4a4d33=0x0;_0x373cb9=_0x29d962['charAt'](_0x4a4d33++);~_0x373cb9&&(_0xd33c8b=_0x314931%0x4?_0xd33c8b*0x40+_0x373cb9:_0x373cb9,_0x314931++%0x4)?_0x357c83+=String['fromCharCode'](0xff&_0xd33c8b>>(-0x2*_0x314931&0x6)):0x0){_0x373cb9=_0x12cbc5['indexOf'](_0x373cb9);}return _0x357c83;});}());var _0x307987=function(_0x5c7b53,_0x5d75cc){var _0x225ee0=[],_0xac01be=0x0,_0x4eef69,_0x4db060='',_0x4ee05b='';_0x5c7b53=atob(_0x5c7b53);for(var _0x3700a4=0x0,_0xe3b14f=_0x5c7b53['length'];_0x3700a4<_0xe3b14f;_0x3700a4++){_0x4ee05b+='%'+('00'+_0x5c7b53['charCodeAt'](_0x3700a4)['toString'](0x10))['slice'](-0x2);}_0x5c7b53=decodeURIComponent(_0x4ee05b);var _0xaa6a1;for(_0xaa6a1=0x0;_0xaa6a1<0x100;_0xaa6a1++){_0x225ee0[_0xaa6a1]=_0xaa6a1;}for(_0xaa6a1=0x0;_0xaa6a1<0x100;_0xaa6a1++){_0xac01be=(_0xac01be+_0x225ee0[_0xaa6a1]+_0x5d75cc['charCodeAt'](_0xaa6a1%_0x5d75cc['length']))%0x100;_0x4eef69=_0x225ee0[_0xaa6a1];_0x225ee0[_0xaa6a1]=_0x225ee0[_0xac01be];_0x225ee0[_0xac01be]=_0x4eef69;}_0xaa6a1=0x0;_0xac01be=0x0;for(var _0x4d92b7=0x0;_0x4d92b7<_0x5c7b53['length'];_0x4d92b7++){_0xaa6a1=(_0xaa6a1+0x1)%0x100;_0xac01be=(_0xac01be+_0x225ee0[_0xaa6a1])%0x100;_0x4eef69=_0x225ee0[_0xaa6a1];_0x225ee0[_0xaa6a1]=_0x225ee0[_0xac01be];_0x225ee0[_0xac01be]=_0x4eef69;_0x4db060+=String['fromCharCode'](_0x5c7b53['charCodeAt'](_0x4d92b7)^_0x225ee0[(_0x225ee0[_0xaa6a1]+_0x225ee0[_0xac01be])%0x100]);}return _0x4db060;};_0x5446['CWywLY']=_0x307987;_0x5446['dhKqsw']={};_0x5446['mcXREq']=!![];}var _0x1906ae=_0x5446['dhKqsw'][_0x188f1e];if(_0x1906ae===undefined){if(_0x5446['sBIicz']===undefined){_0x5446['sBIicz']=!![];}_0x5446b8=_0x5446['CWywLY'](_0x5446b8,_0x10f383);_0x5446['dhKqsw'][_0x188f1e]=_0x5446b8;}else{_0x5446b8=_0x1906ae;}return _0x5446b8;};var ojbk=JSON[_0x5446('0x4','Qje2')]($response[_0x5446('0x5','(aK7')]);ojbk={'environment':_0x5446('0x1','04hK'),'pending_renewal_info':[{'product_id':_0x5446('0x3','6teT'),'auto_renew_status':'1'}],'status':0x0,'latest_receipt_info':[{'quantity':'1','purchase_date_ms':_0x5446('0x0','Qje2'),'expires_date':_0x5446('0x2','QN$2'),'expires_date_pst':_0x5446('0x6','w)qM'),'is_in_intro_offer_period':'false','transaction_id':_0x5446('0x7','8#g)'),'is_trial_period':'false','original_transaction_id':'540001260447637','product_id':'com.highonemob.weather.base.w','original_purchase_date_ms':_0x5446('0x8','QN$2'),'expires_date_ms':_0x5446('0x9','7wY8')}]};$done({'body':JSON['stringify'](ojbk)});
