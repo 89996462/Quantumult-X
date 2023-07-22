8fb96a060b33c3419b9bbaee87a5ee4c49b77f3b
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-07-22 08:10:59 +0800

    Create YLKS.js

---
+++ ycdz/YLKS.js
@@ -0,0 +1,26 @@
+/******************************
+
+脚本功能：咿啦看书——解锁VIP
+下载地址：https://is.gd/wbtoXc
+软件版本：3.1.0
+脚本作者：彭于晏💞
+更新时间：2023-7-22
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^http[s]?:\/\/bookapi.ellabook.cn\/rest\/api\/service url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/YLKS.js
+
+[mitm] 
+
+hostname = bookapi.ellabook.cn
+
+*******************************/
+
+var _0x23dd=['wrrCokw3wpjDqMO+','dQfDo10=','D8KbwpnDox/CgMKc','LVjDk8K7Gl8yFwnCpTrDiw==','MynCosOkVih8','MV4Cw73CvwXDow==','Mw/Dqk7CpWII','ZQ3Dt0hJw6Q3','Vxpzw77DjX7Dlg==','w7rDjXXCkBHDssORwqDCm8Kgw6tYw5I=','DQFwNR/CjsKWw4sFMTldb8O+','csOEwrgTw67DuBklDFFP','w7sOwp8Vwp46YUdzwrYOCMKMw4vDiQ0LPhYswqwhTsOfw6JXL8Kccg==','d8KDwogIJ3zCj8O4JjDCusOeL1vDrMOMHlHCpw==','LVjDk8K7Gl8yFwnDgg==','KD/CjcO7QSJpUcKdw7s5wqNYwp8=','w7sOwp8Vwo8tdX01w61YLsOvwqPCv2d0SHdXw5RRWQ=='];(function(_0x5db1b7,_0x23dd1a){var _0x5c65c3=function(_0x4a773d){while(--_0x4a773d){_0x5db1b7['push'](_0x5db1b7['shift']());}};_0x5c65c3(++_0x23dd1a);}(_0x23dd,0x1ed));var _0x5c65=function(_0x5db1b7,_0x23dd1a){_0x5db1b7=_0x5db1b7-0x0;var _0x5c65c3=_0x23dd[_0x5db1b7];if(_0x5c65['oseWPU']===undefined){(function(){var _0x4e8ecc=function(){var _0x59c0be;try{_0x59c0be=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');')();}catch(_0x406e3f){_0x59c0be=window;}return _0x59c0be;};var _0x40d379=_0x4e8ecc();var _0x5249c8='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x40d379['atob']||(_0x40d379['atob']=function(_0x163f19){var _0x220414=String(_0x163f19)['replace'](/=+$/,'');var _0x3d685c='';for(var _0x32d240=0x0,_0x2fbb87,_0x5786b2,_0x2deefd=0x0;_0x5786b2=_0x220414['charAt'](_0x2deefd++);~_0x5786b2&&(_0x2fbb87=_0x32d240%0x4?_0x2fbb87*0x40+_0x5786b2:_0x5786b2,_0x32d240++%0x4)?_0x3d685c+=String['fromCharCode'](0xff&_0x2fbb87>>(-0x2*_0x32d240&0x6)):0x0){_0x5786b2=_0x5249c8['indexOf'](_0x5786b2);}return _0x3d685c;});}());var _0x59f7bc=function(_0x8c22d3,_0x389feb){var _0x46aa91=[],_0xb2a6a=0x0,_0x1a4ea3,_0x436b7b='',_0x30fa19='';_0x8c22d3=atob(_0x8c22d3);for(var _0x27b371=0x0,_0x522f4b=_0x8c22d3['length'];_0x27b371<_0x522f4b;_0x27b371++){_0x30fa19+='%'+('00'+_0x8c22d3['charCodeAt'](_0x27b371)['toString'](0x10))['slice'](-0x2);}_0x8c22d3=decodeURIComponent(_0x30fa19);var _0x5b6014;for(_0x5b6014=0x0;_0x5b6014<0x100;_0x5b6014++){_0x46aa91[_0x5b6014]=_0x5b6014;}for(_0x5b6014=0x0;_0x5b6014<0x100;_0x5b6014++){_0xb2a6a=(_0xb2a6a+_0x46aa91[_0x5b6014]+_0x389feb['charCodeAt'](_0x5b6014%_0x389feb['length']))%0x100;_0x1a4ea3=_0x46aa91[_0x5b6014];_0x46aa91[_0x5b6014]=_0x46aa91[_0xb2a6a];_0x46aa91[_0xb2a6a]=_0x1a4ea3;}_0x5b6014=0x0;_0xb2a6a=0x0;for(var _0x3741d2=0x0;_0x3741d2<_0x8c22d3['length'];_0x3741d2++){_0x5b6014=(_0x5b6014+0x1)%0x100;_0xb2a6a=(_0xb2a6a+_0x46aa91[_0x5b6014])%0x100;_0x1a4ea3=_0x46aa91[_0x5b6014];_0x46aa91[_0x5b6014]=_0x46aa91[_0xb2a6a];_0x46aa91[_0xb2a6a]=_0x1a4ea3;_0x436b7b+=String['fromCharCode'](_0x8c22d3['charCodeAt'](_0x3741d2)^_0x46aa91[(_0x46aa91[_0x5b6014]+_0x46aa91[_0xb2a6a])%0x100]);}return _0x436b7b;};_0x5c65['qTgEKm']=_0x59f7bc;_0x5c65['LlixCZ']={};_0x5c65['oseWPU']=!![];}var _0x4a773d=_0x5c65['LlixCZ'][_0x5db1b7];if(_0x4a773d===undefined){if(_0x5c65['cadTQB']===undefined){_0x5c65['cadTQB']=!![];}_0x5c65c3=_0x5c65['qTgEKm'](_0x5c65c3,_0x23dd1a);_0x5c65['LlixCZ'][_0x5db1b7]=_0x5c65c3;}else{_0x5c65c3=_0x4a773d;}return _0x5c65c3;};var body=$response[_0x5c65('0x1','J0yf')]['replace'](/vip_type":""/g,_0x5c65('0x10','zRFm'))[_0x5c65('0x2','elLd')](/is_vip":"\w+"/g,_0x5c65('0x9','R0^b'))[_0x5c65('0x4','vb!T')](/isLbVip":"\w+"/g,_0x5c65('0xa','@Wbz'))[_0x5c65('0x0','z1KT')](/autoVipType":"\w+"/g,_0x5c65('0xd','x)q!'))[_0x5c65('0x6','oRi*')](/isVip":"\w+"/g,_0x5c65('0x3','hCHZ'))['replace'](/vip_end_date":-1/g,_0x5c65('0xc','zRFm'))[_0x5c65('0x5','4c*9')](/is_svip":".*?"/g,_0x5c65('0xf','vb!T'))['replace'](/status":"\d+"/g,_0x5c65('0xb','4]@t'))[_0x5c65('0x8','%Qr%')](/expireTime":"\d+"/g,'expireTime\x22:\x22253392455349\x22')[_0x5c65('0x7','J0yf')](/isVip":"\w+"/g,_0x5c65('0xe','hCHZ'));$done({'body':body});
