fe23fd1080dfb16774bdd16ee11f3684f052f9b4
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-08-15 13:50:53 +0800

    Update pilipili.js

--- ghs/pilipili.js
+++ ghs/pilipili.js
@@ -18,7 +18,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^http[s]?:\/\/120play.(fuhuida|riyufanyi|longyuandingyi).*\/(videos.|watch|static|upload|watch[0-9]+)\/.*\/.*\.m3u8\?auth_key.*$ url script-request-header https://raw.githubusercontent.com/89996462/Quantumult-X/main/ghs/pilipili.js
+^http[s]?:\/\/(10play|120play)(fuhuida|riyufanyi|longyuandingyi).*\/(videos.|watch|static|upload|watch[0-9]+)\/.*\/.*\.m3u8\?auth_key.*$ url script-request-header https://raw.githubusercontent.com/89996462/Quantumult-X/main/ghs/pilipili.js
 
 [mitm] 
 
