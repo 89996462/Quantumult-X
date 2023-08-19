49f580a6289be3983150a685c71daeabe0426d7a
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-08-19 15:26:16 +0800

    Update dasyibalang.js

--- ycdz/dasyibalang.js
+++ ycdz/dasyibalang.js
@@ -18,7 +18,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^http[s]?:\/\/(integral2|w|mix2|api)\.dasyibalang\.com\/v2\/User url script-response-body 88811.js
+^http[s]?:\/\/(integral2|w|mix2|api)\.dasyibalang\.com\/v2\/User url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/dasyibalang.js
 
 [mitm] 
 
