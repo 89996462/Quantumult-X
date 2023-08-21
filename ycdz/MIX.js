d817b76d0272e23a321385fdd636e3e146ff8d78
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-08-21 23:36:51 +0800

    Update MIX.js

--- ycdz/MIX.js
+++ ycdz/MIX.js
@@ -1,25 +1,28 @@
 /******************************
 
-脚本功能：MX滤镜大师+解锁订阅
+脚本名称: MX滤镜大师+解锁VIP权限——不显示VIP
 下载地址：https://is.gd/PCaWgW
-软件版本：5.2.15
+软件版本：5.2.21
 脚本作者：彭于晏💞
-更新时间：2022-10-1
+更新时间：2023-8-21
 问题反馈：QQ+89996462
 QQ会员群：779392027💞
 TG反馈群：https://t.me/plus8889
 TG频道群：https://t.me/py996
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
+更多资源请微信搜索小程序【屌丝博客】
+
 *******************************
 
 [rewrite_local]
 
-https?:\/\/bmall\.camera360\.com\/api\/mix\/recovery url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/MIX.js
+^https:\/\/cdn-bm\.camera360\.com\/api\/mix\/getinfo url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/MIX.js
 
-[mitm]
+[mitm] 
 
-hostname = bmall.camera360.com
+hostname = cdn-bm.camera360.com
+
 
 **************************/
 
