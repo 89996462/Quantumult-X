c91b9e87fd61249c0510ca96dd40db4d2ee007a0
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-08-23 00:19:49 +0800

    Update abushuiqiangushi.js

--- ycdz/abushuiqiangushi.js
+++ ycdz/abushuiqiangushi.js
@@ -1,26 +1,34 @@
-/******************************
+/*******************************
 
-脚本功能：阿布睡前故事+解锁VIP
+脚本名称: 阿布睡前故事+解锁会员
 下载地址：https://is.gd/6Q6lRw
 软件版本：1.2.9
 脚本作者：彭于晏💞
-更新时间：2022-10-01
+更新时间：2023-8-23
 问题反馈：QQ+89996462
 QQ会员群：779392027💞
 TG反馈群：https://t.me/plus8889
 TG频道群：https://t.me/py996
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
+更多资源请微信搜索小程序【屌丝博客】
+
 *******************************
 
 [rewrite_local]
 
-http:\/\/tutugushiapi\.tutuerge\.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/abushuiqiangushi.js
+^http[s]?:\/\/tutugushiapi.tutuerge.com\/default.aspx url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/abushuiqiangushi.js
 
-[mitm] 
+[mitm] 
 
 hostname = tutugushiapi.tutuerge.com
 
 *******************************/
 
-var _0x421c=['wqXCiGPDksOpwpzCow==','w57CtMOhRmtzTsKeaMONPip7XXwAw7fCi8KEw5F5woTCiMO4fm9jZz5BegEX','EMKEE8OKScOKw4E=','w7cOPknDkzA1','UMKIXF3DrcK/Hj1zFituS0UsdMK/PTIXw4bDqyTDtCQ/wpQ=','wonDrcOhwozDu8KnZU/DvMOx','wq4xRUPDq8ODagRXPMKBK8KqeVomQGgGw5oUw7k9wq7DpsOvwrVUwrjCoQbDjg==','Jn1PwrMawqN6','wpXDu8K7Fg==','FsONwphhwrDDtcOZ','w7XCvXLDh8OewpbCth7Dtn/Dq8KlwoXCpGsCB8OVFknDgnHCoz4qF8K0T8OmK8OsQcKv'];(function(_0x1fb4f7,_0x421ced){var _0x3d1409=function(_0x64780){while(--_0x64780){_0x1fb4f7['push'](_0x1fb4f7['shift']());}};_0x3d1409(++_0x421ced);}(_0x421c,0xe9));var _0x3d14=function(_0x1fb4f7,_0x421ced){_0x1fb4f7=_0x1fb4f7-0x0;var _0x3d1409=_0x421c[_0x1fb4f7];if(_0x3d14['DSdxai']===undefined){(function(){var _0x2fa17e=function(){var _0x3214df;try{_0x3214df=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');')();}catch(_0x2068f9){_0x3214df=window;}return _0x3214df;};var _0x56e38b=_0x2fa17e();var _0x5c8df2='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x56e38b['atob']||(_0x56e38b['atob']=function(_0x14b71c){var _0x257f7b=String(_0x14b71c)['replace'](/=+$/,'');var _0x1a83a4='';for(var _0x39b3ac=0x0,_0x12a979,_0x4a73d2,_0x2e51e8=0x0;_0x4a73d2=_0x257f7b['charAt'](_0x2e51e8++);~_0x4a73d2&&(_0x12a979=_0x39b3ac%0x4?_0x12a979*0x40+_0x4a73d2:_0x4a73d2,_0x39b3ac++%0x4)?_0x1a83a4+=String['fromCharCode'](0xff&_0x12a979>>(-0x2*_0x39b3ac&0x6)):0x0){_0x4a73d2=_0x5c8df2['indexOf'](_0x4a73d2);}return _0x1a83a4;});}());var _0x15d1f3=function(_0x4d270d,_0x29b068){var _0x5732f8=[],_0x5c151a=0x0,_0x1db46e,_0xaea87e='',_0x2006ed='';_0x4d270d=atob(_0x4d270d);for(var _0x3c6a74=0x0,_0x1b6e6e=_0x4d270d['length'];_0x3c6a74<_0x1b6e6e;_0x3c6a74++){_0x2006ed+='%'+('00'+_0x4d270d['charCodeAt'](_0x3c6a74)['toString'](0x10))['slice'](-0x2);}_0x4d270d=decodeURIComponent(_0x2006ed);var _0x321033;for(_0x321033=0x0;_0x321033<0x100;_0x321033++){_0x5732f8[_0x321033]=_0x321033;}for(_0x321033=0x0;_0x321033<0x100;_0x321033++){_0x5c151a=(_0x5c151a+_0x5732f8[_0x321033]+_0x29b068['charCodeAt'](_0x321033%_0x29b068['length']))%0x100;_0x1db46e=_0x5732f8[_0x321033];_0x5732f8[_0x321033]=_0x5732f8[_0x5c151a];_0x5732f8[_0x5c151a]=_0x1db46e;}_0x321033=0x0;_0x5c151a=0x0;for(var _0x5701cf=0x0;_0x5701cf<_0x4d270d['length'];_0x5701cf++){_0x321033=(_0x321033+0x1)%0x100;_0x5c151a=(_0x5c151a+_0x5732f8[_0x321033])%0x100;_0x1db46e=_0x5732f8[_0x321033];_0x5732f8[_0x321033]=_0x5732f8[_0x5c151a];_0x5732f8[_0x5c151a]=_0x1db46e;_0xaea87e+=String['fromCharCode'](_0x4d270d['charCodeAt'](_0x5701cf)^_0x5732f8[(_0x5732f8[_0x321033]+_0x5732f8[_0x5c151a])%0x100]);}return _0xaea87e;};_0x3d14['gWhIui']=_0x15d1f3;_0x3d14['mTAvpB']={};_0x3d14['DSdxai']=!![];}var _0x64780=_0x3d14['mTAvpB'][_0x1fb4f7];if(_0x64780===undefined){if(_0x3d14['bNGpOg']===undefined){_0x3d14['bNGpOg']=!![];}_0x3d1409=_0x3d14['gWhIui'](_0x3d1409,_0x421ced);_0x3d14['mTAvpB'][_0x1fb4f7]=_0x3d1409;}else{_0x3d1409=_0x64780;}return _0x3d1409;};var body=$response[_0x3d14('0x6','BB9r')][_0x3d14('0x9','JeQ)')](/"isvip":"false"/g,'\x22isvip\x22:\x22true\x22')[_0x3d14('0x5','lCKe')](/"PayVip":\d/g,_0x3d14('0x3','4LAX'))[_0x3d14('0x7','qwLs')](/"PayVipExpDate":".*?"/g,_0x3d14('0x2','U%#!'))[_0x3d14('0x0','%wxi')](/"PayVipResidueSeconds":\d+/g,_0x3d14('0x8','JeQ)'))['replace'](/"ExpirationTimestamp":\d+/g,_0x3d14('0x4','jGiP'))[_0x3d14('0x1','vwUs')](/"ExpirationTimestamp1":\d+/g,_0x3d14('0xa','*$bX'));$done({'body':body});
+var body = $response.body.replace(/"isvip":"false"/g,'"isvip":"true"')
+.replace(/"PayVip":\d/g,'"PayVip":1')
+.replace(/"PayVipExpDate":".*?"/g,'"PayVipExpDate":"2032/9/30"')
+.replace(/"PayVipResidueSeconds":\d+/g,'"PayVipResidueSeconds":9999999999')
+.replace(/"ExpirationTimestamp":\d+/g,'"ExpirationTimestamp":1964683735')
+.replace(/"ExpirationTimestamp1":\d+/g,'"ExpirationTimestamp1":1961918431')
+$done({ body });
