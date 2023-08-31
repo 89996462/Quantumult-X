86e18d58d8a6c3e1b22928b58b86d1d300bf1b1f
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-08-31 23:54:48 +0800

    Create znspkt.js

---
+++ ycdz/znspkt.js
@@ -0,0 +1,87 @@
+/*************************************
+
+项目名称：智能视频抠图——解锁订阅
+下载地址：https://is.gd/tjV29W
+软件版本：2.0
+脚本作者：彭于晏💞
+更新时间：2023-9-1
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+更多资源请微信搜索小程序【屌丝博客】
+
+
+**************************************
+
+[rewrite_local]
+
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/znspkt.js
+
+[mitm]
+
+hostname = buy.itunes.apple.com
+
+*************************************/
+
+
+var anni = {};
+var anni01 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+var headers = {};
+for (var key in $request.headers) {
+  const reg = /^[a-z]+$/;
+  if (key === "User-Agent" && !reg.test(key)) {
+    var lowerkey = key.toLowerCase();
+    $request.headers[lowerkey] = $request.headers[key];
+    delete $request.headers[key];
+  }
+}
+var UA = $request.headers['user-agent'];
+var uaProductMapping = {
+  'VideoPeopleSegmetation': {product_id: 'com.bestmusicvideo.videobackground.1yearvip'},
+};
+var receipt = {
+  "quantity": "1",
+  "purchase_date_ms": "1686776705000",
+  "expires_date": "2099-12-31 05:05:05 Etc\/GMT",
+  "expires_date_pst": "2099-12-31 05:05:05 America\/Los_Angeles",
+  "is_in_intro_offer_period": "false",
+  "transaction_id": "999999999999999",
+  "is_trial_period": "false",
+  "original_transaction_id": "999999999999999",
+  "purchase_date": "2023-06-15 05:05:05 Etc\/GMT",
+  "product_id": "888888888888888",
+  "original_purchase_date_pst": "2023-06-15 05:05:05 America\/Los_Angeles",
+  "in_app_ownership_type": "PURCHASED",
+  "subscription_group_identifier": "20877951",
+  "original_purchase_date_ms": "1686776705000",
+  "web_order_line_item_id": "999999999999999",
+  "expires_date_ms": "4102347905000",
+  "purchase_date_pst": "2023-06-15 05:05:05 America\/Los_Angeles",
+  "original_purchase_date": "2023-06-15 05:05:05 Etc\/GMT"
+}
+var renewal = {
+  "expiration_intent": "1",
+  "product_id": "888888888888888",
+  "is_in_billing_retry_period": "0",
+  "auto_renew_product_id": "888888888888888",
+  "original_transaction_id": "999999999999999",
+  "auto_renew_status": "0"
+}
+for (var uaKey in uaProductMapping) {
+  if (UA && UA.includes(uaKey)) {
+    var productInfo = uaProductMapping[uaKey];
+    var product_id = productInfo.product_id;
+    receipt.product_id = product_id;
+    renewal.product_id = product_id;
+    renewal.auto_renew_product_id = product_id;
+    anni01.receipt.in_app = [receipt];
+    anni01.latest_receipt_info = [receipt];
+    anni.pending_renewal_info = [renewal];
+    break;
+  }
+}
+anni = anni01;
+$done({ body: JSON.stringify(anni) });
