a5a08db7939ffad99ba1e41bd186fc713ffd6839
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-09-02 16:53:55 +0800

    Create ChatAl40.js

---
+++ ycdz/ChatAl40.js
@@ -0,0 +1,26 @@
+/******************************
+
+脚本功能：ChatAl 4.0中文版——解锁VIP
+下载地址：https://is.gd/4BXHtS
+软件版本：3.1.0
+脚本作者：彭于晏💞
+更新时间：2023-9-2
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+更多资源请微信搜索小程序【屌丝博客】
+
+*******************************/
+
+[rewrite_local]
+
+(?<=vip=)false url 302 true
+
+^https:\/\/www\.longstargpt\.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ChatAl40.js
+
+[mitm] 
+
+hostname = www.longstargpt.com
