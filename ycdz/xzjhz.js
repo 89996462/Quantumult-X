0d09de4a6cb58c8eac574377ae3d6031f0f34f7a
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-09-02 10:11:39 +0800

    Update xzjhz.js

--- ycdz/xzjhz.js
+++ ycdz/xzjhz.js
@@ -1,25 +1,27 @@
 /******************************
 
-脚本功能：小组件盒子——解锁VIP
+脚本名称: 小组件盒子——解锁VIP
 下载地址：https://is.gd/xVJl4X
-软件版本：0.17.1
+软件版本：1.4.3
 脚本作者：彭于晏💞
-更新时间：2022-11-09
+更新时间：2023-9-2
 问题反馈：QQ+89996462
 QQ会员群：779392027💞
 TG反馈群：https://t.me/plus8889
 TG频道群：https://t.me/py996
-使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+更多资源请微信搜索小程序【屌丝博客】
 
 *******************************
 
 [rewrite_local]
 
-^https:\/\/widget-box-api\.codefuture\.top\/v1\/users\/me url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/xzjhz.js
+^https:\/\/api\.widgetbox\.top\/v1\/users\/me url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/xzjhz.js
 
 [mitm] 
 
-hostname = widget-box-api.codefuture.top
+hostname = api.widgetbox.top
 
 *******************************/
 
