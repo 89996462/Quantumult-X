8ccad98b2b271aad4e8e42c3cae5dd1ec858a56e
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-09-04 21:57:08 +0800

    Update MGTV1.js

--- gx/MGTV1.js
+++ gx/MGTV1.js
@@ -12,11 +12,6 @@ TG频道群：https://t.me/py996
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 更多资源请微信搜索小程序【屌丝博客】
-
-/********************************
-
-^http[s]?:\/\/mobile\.api\.mgtv\.com\/v[0-9]\/(playlist|video\/album|video\/relative|video\/list).*$ url script-request-header https://raw.githubusercontent.com/89996462/Quantumult-X/main/gx/MGTV1.js
-
 ********************************/
 
 var { Cookie, 'x-r-i': xri, 'x-l-r-i': xlri, ...rest } = $request.headers;
