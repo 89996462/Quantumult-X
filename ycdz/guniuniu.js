6de2390a6cdf5b2dcac6ab3fdc2ef949aac6df39
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-10-17 23:57:25 +0800

    Create guniuniu.js

---
+++ ycdz/guniuniu.js
@@ -0,0 +1,31 @@
+/******************************
+
+脚本功能：题材库——解锁VIP权限
+下载地址：微信小程序
+软件版本：9.9.9
+脚本作者：彭于晏💞
+更新时间：2023-10-5
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+
+*******************************
+
+[rewrite_local]
+
+^http[s]?:\/\/miniapp.guniuniu.com\/api\/mini-app\/user url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/guniuniu.js
+
+[mitm] 
+
+hostname = miniapp.guniuniu.com
+
+*******************************/
+
+var body = $response.body.replace(/vipNotifyStatus":\d/g,'vipNotifyStatus":1')
+.replace(/vipType":"\d"/g,'vipType":"1"')
+.replace(/vipTime":\w+/g,'vipTime":"9999-10-20 11:30:33"')
+$done({ body });
+
