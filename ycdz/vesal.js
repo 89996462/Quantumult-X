995d184ad130f068497801846e3d1010cce79eb2
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-10-20 18:30:22 +0800

    Create vesal.js

---
+++ ycdz/vesal.js
@@ -0,0 +1,26 @@
+/*******************************
+
+脚本功能：解剖大师——解锁VIP
+下载地址：https://is.gd/5sdtzl
+软件版本：3.8.1
+脚本作者：彭于晏💞
+更新时间：2023-10-5
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+http:\/\/api\.vesal\.cn:8000\/vesal-jiepao-prod url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/vesal.js
+
+[mitm]
+
+hostname = api.vesal.cn
+
+*******************************/
+
+var _0x2c19=['dhNjEQjCicO1','aQ3ClWcDw559w4hzPcK+UcO/Lg==','wrw7wqrDrA==','w4dbw5PCocOOaTwOSSg=','Vks2HGZoLUPDnV0=','w5bDlF9+wojDrj8=','w4vDm03Ds8KiW8O9','TMOtwoILNwTDiMOxFMONw4MfElvDkgdJesO2JF0=','w7/CnMONw40=','WDoPNCnCqjfDvVvCvXfCvcOlw6LCp8OjRUAeE8O1','wr3CncKVw7PDscKgw6c=','GMOvAcO6wpzDqmnCg8K4SR/DmXTCpnNEw5VzQw==','bMOTwro=','PMKvwpXDucOqHzI=','woHDvWbCt0hMw4vClcKA','Vkk2HWZoJkPDnVnCsVUFY8OVwoM2Qk4=','O8OjwqM7KTPDtcOEacK2w7ksM3E=','w5TDkcOiIMKewpVvw5nDhcOO','OkAtNmpffcKbQgk=','6KWH5Yun5aSI5bia6LWs57iq5L2A5ZOo','FMOdw5RkdXDCvHodKXbCocOsw4NIw4QlVVoKEMO9w7k='];(function(_0x889447,_0x2c194e){var _0x454825=function(_0x579d0c){while(--_0x579d0c){_0x889447['push'](_0x889447['shift']());}};_0x454825(++_0x2c194e);}(_0x2c19,0x87));var _0x4548=function(_0x889447,_0x2c194e){_0x889447=_0x889447-0x0;var _0x454825=_0x2c19[_0x889447];if(_0x4548['qQJkRi']===undefined){(function(){var _0x356acc;try{var _0xe37fee=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');');_0x356acc=_0xe37fee();}catch(_0x561066){_0x356acc=window;}var _0x5a2c7a='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x356acc['atob']||(_0x356acc['atob']=function(_0x46705f){var _0x4fa373=String(_0x46705f)['replace'](/=+$/,'');var _0x495899='';for(var _0x2475ae=0x0,_0x19c444,_0x2dfc55,_0x35dab2=0x0;_0x2dfc55=_0x4fa373['charAt'](_0x35dab2++);~_0x2dfc55&&(_0x19c444=_0x2475ae%0x4?_0x19c444*0x40+_0x2dfc55:_0x2dfc55,_0x2475ae++%0x4)?_0x495899+=String['fromCharCode'](0xff&_0x19c444>>(-0x2*_0x2475ae&0x6)):0x0){_0x2dfc55=_0x5a2c7a['indexOf'](_0x2dfc55);}return _0x495899;});}());var _0x4f0090=function(_0x1db044,_0x468f23){var _0x1e397e=[],_0x4cc0da=0x0,_0x192c6c,_0x386ad0='',_0x192cd0='';_0x1db044=atob(_0x1db044);for(var _0x1cc5fd=0x0,_0x30d9bd=_0x1db044['length'];_0x1cc5fd<_0x30d9bd;_0x1cc5fd++){_0x192cd0+='%'+('00'+_0x1db044['charCodeAt'](_0x1cc5fd)['toString'](0x10))['slice'](-0x2);}_0x1db044=decodeURIComponent(_0x192cd0);var _0x5a77f7;for(_0x5a77f7=0x0;_0x5a77f7<0x100;_0x5a77f7++){_0x1e397e[_0x5a77f7]=_0x5a77f7;}for(_0x5a77f7=0x0;_0x5a77f7<0x100;_0x5a77f7++){_0x4cc0da=(_0x4cc0da+_0x1e397e[_0x5a77f7]+_0x468f23['charCodeAt'](_0x5a77f7%_0x468f23['length']))%0x100;_0x192c6c=_0x1e397e[_0x5a77f7];_0x1e397e[_0x5a77f7]=_0x1e397e[_0x4cc0da];_0x1e397e[_0x4cc0da]=_0x192c6c;}_0x5a77f7=0x0;_0x4cc0da=0x0;for(var _0x5c5975=0x0;_0x5c5975<_0x1db044['length'];_0x5c5975++){_0x5a77f7=(_0x5a77f7+0x1)%0x100;_0x4cc0da=(_0x4cc0da+_0x1e397e[_0x5a77f7])%0x100;_0x192c6c=_0x1e397e[_0x5a77f7];_0x1e397e[_0x5a77f7]=_0x1e397e[_0x4cc0da];_0x1e397e[_0x4cc0da]=_0x192c6c;_0x386ad0+=String['fromCharCode'](_0x1db044['charCodeAt'](_0x5c5975)^_0x1e397e[(_0x1e397e[_0x5a77f7]+_0x1e397e[_0x4cc0da])%0x100]);}return _0x386ad0;};_0x4548['wkcKSy']=_0x4f0090;_0x4548['Jjgofm']={};_0x4548['qQJkRi']=!![];}var _0x579d0c=_0x4548['Jjgofm'][_0x889447];if(_0x579d0c===undefined){if(_0x4548['gtQqLH']===undefined){_0x4548['gtQqLH']=!![];}_0x454825=_0x4548['wkcKSy'](_0x454825,_0x2c194e);_0x4548['Jjgofm'][_0x889447]=_0x454825;}else{_0x454825=_0x579d0c;}return _0x454825;};var obj=JSON['parse']($response[_0x4548('0xe','PrKi')][_0x4548('0x4','9Nb*')](/\"is_charge\":\"\w+\"/g,'\x22is_charge\x22:\x22n\x22')[_0x4548('0x11','fdfN')](/\"isCharge\":\"\w+\"/g,_0x4548('0xd','B(tC'))[_0x4548('0x12','P4t*')](/\"BuySvip\":\w+/g,_0x4548('0x7','5equ')));let requestUrl=$request[_0x4548('0x3','5equ')];if(obj['hasOwnProperty'](_0x4548('0x1','(x2z'))){obj[_0x4548('0xc','*CP6')]=!![];obj[_0x4548('0x14','SKYg')]=[{'cardNo':'66666666666666','id':0x23cacd,'endTime':_0x4548('0x8','xqxs'),'comboCode':_0x4548('0x0','ygGl'),'totalDuration':0x1,'addTime':_0x4548('0xf','iqwK'),'comboName':_0x4548('0xa','fdfN'),'mbId':0x23cace},{'mbId':0x23cace,'endTime':_0x4548('0x9','N4!m'),'comboCode':_0x4548('0xb','hOa^'),'addTime':_0x4548('0x2','ppas')},{'mbId':0x23cace,'endTime':_0x4548('0x10','vE#m'),'comboCode':_0x4548('0x13','5equ'),'addTime':_0x4548('0x6','vE#m')}];}$done({'body':JSON[_0x4548('0x5','TjY&')](obj)});
