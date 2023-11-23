a797d4fbb4ea9ed142d0a58e393ba1f34e0b9177
彭于晏 <65338783+89996462@users.noreply.github.com>
2023-11-24 02:02:19 +0800

    Update kp.js

--- ycdz/kp.js
+++ ycdz/kp.js
@@ -2,9 +2,9 @@
 
 脚本功能：可拍 - Al换脸变装视频—解锁VIP
 下载地址：https://is.gd/sAdANg
-软件版本：2.4.5
+软件版本：2.7.0
 脚本作者：彭于晏💞
-更新时间：2022-11-17
+更新时间：2023-11-25
 问题反馈：QQ+89996462
 QQ会员群：779392027💞
 TG反馈群：https://t.me/plus8889
@@ -13,16 +13,18 @@ TG频道群：https://t.me/py996
 
 *******************************
 
+
 [rewrite_local]
 
-http:\/\/fxshot-api\.afunapp\.com\/order_api\/apple_query url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/kp.js
+^http[s]?:\/\/fxshot-api.afunapp.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/kp.js
 
-[mitm] 
+[mitm] 
 
 hostname = fxshot-api.afunapp.com
 
 *******************************/
 
-body = $response.body.replace(/\"vip_remain_time":\d+/g, '\"vip_remain_time":4098497565')
+var body = $response.body.replace(/vip_remain_time":\d+/g,'vip_remain_time":4098497565')
+.replace(/gender":\d/g,'gender":1')
+$done({ body });
 
-$done({body});
