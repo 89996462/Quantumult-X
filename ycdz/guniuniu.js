67eb6357fdc171a8ae9a901dca679e414e15312c
彭于晏 <65338783+89996462@users.noreply.github.com>
2024-03-21 10:47:28 +0800

    Update guniuniu.js

--- ycdz/guniuniu.js
+++ ycdz/guniuniu.js
@@ -4,7 +4,7 @@
 下载地址：微信小程序
 软件版本：9.9.9
 脚本作者：彭于晏💞
-更新时间：2023-10-5
+更新时间：2024-10-5
 问题反馈：QQ+89996462
 QQ会员群：779392027💞
 TG反馈群：https://t.me/plus8889
@@ -16,9 +16,9 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^http[s]?:\/\/miniapp.guniuniu.com\/api\/mini-app\/user url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/guniuniu.js
+^https:\/\/miniapp\.guniuniu\.com\/api\/app\/user url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/guniuniu.js
 
-[mitm] 
+[mitm] 
 
 hostname = miniapp.guniuniu.com
 
