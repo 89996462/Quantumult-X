31af63f21ff6918e9a64b81a9f644970aa2534ef
彭于晏 <65338783+89996462@users.noreply.github.com>
2024-04-11 16:33:08 +0800

    Create yifanxinmei1.js

---
+++ ycdz/yifanxinmei1.js
@@ -0,0 +1,28 @@
+/******************************
+
+脚本功能：今日水印拍照相机——解锁VIP
+下载地址：https://t.cn/A6TCyiNC
+软件版本：1.7
+脚本作者：彭于晏💞
+更新时间：2024-8-15
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^http[s]?:\/\/water-fst.yifanxinmei.com\/api\/guests\/login url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/yifanxinmei1.js
+
+[mitm] 
+
+hostname = water-fst.yifanxinmei.com
+
+*******************************/
+
+var body = $response.body.replace(/vip_expire_date":\d+/g,'vip_expire_date":8888888888000')
+.replace(/vip":\d/g,'vip":1')
+$done({ body });
