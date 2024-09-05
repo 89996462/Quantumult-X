c6d6c38e0aaa7729ad38532cb0bb5fab5262aae6
彭于晏 <65338783+89996462@users.noreply.github.com>
2024-09-05 10:18:52 +0800

    Create iosznsmw.js

---
+++ ycdz/iosznsmw.js
@@ -0,0 +1,31 @@
+/*******************************
+
+脚本功能：智能扫描王——解锁VIP
+下载地址：https://is.gd/8yHbBx
+软件版本：1.4
+脚本作者：彭于晏💞
+更新时间：2024-10—8
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/iosznsmw\.lmscanner\.sanwubeixin\.cn\/ct\/user\/info url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/iosznsmw.js
+
+[mitm] 
+
+hostname = iosznsmw.lmscanner.sanwubeixin.cn
+
+
+*******************************/
+
+var body = $response.body.replace(/isVip":\w+/g,'isVip":true')
+.replace(/vip_time":\d+/g,'vip_time":8888888888')
+.replace(/vip_type":\d+/g,'vip_type":1')
+.replace(/username":".*?"/g,'username":"彭于晏Crack"')
+$done({ body });
