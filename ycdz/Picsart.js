969e300fbe36e621b449d767d3b34084bbc19605
彭于晏 <65338783+89996462@users.noreply.github.com>
2025-03-13 13:22:18 +0800

    Update Picsart.js

--- ycdz/Picsart.js
+++ ycdz/Picsart.js
@@ -2,26 +2,64 @@
 
 脚本功能：Picsart 美易——解锁VIP
 下载地址：https://is.gd/XXUsD9
-软件版本：18.3.3
+软件版本：26.9.2
 脚本作者：彭于晏💞
-更新时间：2022-11-16
+更新时间：2025-3-15
 问题反馈：QQ+89996462
 QQ会员群：779392027💞
 TG反馈群：https://t.me/plus8889
 TG频道群：https://t.me/py996
-使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 *******************************
 
 [rewrite_local]
 
-^https:\/\/api\.meiease\.cn\/shop\/subscription\/validate url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Picsart.js
+^http[s]?:\/\/api.aidimension.cn/shop/subscription/apple/purchases url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Picsart.js
 
-[mitm] 
 
-hostname = api.meiease.cn
+[mitm] 
+hostname = api.aidimension.cn
 
 *******************************/
 
+var objc = JSON.parse($response.body);
+    objc = {
+  "status" : "success",
+  "response" : [
+    {
+      "status" : "SUBSCRIPTION_PURCHASED",
+      "order_id" : "490001314520000",
+      "original_order_id" : "490001314520000",
+      "is_trial" : true,
+      "plan_meta" : {
+        "storage_limit_in_mb" : 20480,
+        "frequency" : "yearly",
+        "scope_id" : "full",
+        "id" : "com.picsart.editor.subscription_yearly",
+        "product_id" : "subscription_yearly",
+        "level" : 2000,
+        "description" : "china",
+        "type" : "renewable",
+        "auto_renew_product_id" : "com.picsart.editor.subscription_yearly",
+        "tier_id" : "gold_old",
+        "permissions" : [
+          "premium_tools_standard",
+          "premium_tools_ai"
+        ]
+      },
+      "limitation" : {
+        "max_count" : 5,
+        "limits_exceeded" : false
+      },
+      "reason" : "ok",
+      "subscription_id" : "com.picsart.editor.subscription_yearly",
+      "is_eligible_for_introductory" : false,
+      "purchase_date" : 1687020148000,
+      "expire_date" : 4092599349000
+    }
+  ]
+}
 
-var _0x5b23=['wpvChi8=','w59Tw7LCoA==','BsKfwo5xw7bDuybCmw==','QcK5w7oSwqvCux4=','H8KAwrBrw5vDvyLCg1s=','w5vDtcKww5kGWsKGX8OQd1ZKSsObwps1wrI=','w4LCr8ODUsO2JsKewpd6wpcXw7IqwpTCqsOTUlI=','w5nCkhY0UBDDjDHCrhh2RzfCj8Of','w5fCj8Kww704w43CmkdbwpnCtBXDvMOFCTRnUQPChMOnwoDCl1UPwoZELkDDkwUAWcKww5wXVsOi','w4BfbgA7CwfCuMOePcOSwr/CisKTw6I=','wqXCrMK6Ow/CnhPDlsKbw4gFDGw=','w5AEc2nDs8O9w5XCvsOfKsKuwrjDoMOIJ186','w7xMFgbDhVkxZA==','w6bDj0rCkQJUM8KzOA==','FjB6w4jDpcKjw4VlPw==','XGHCtcKTw7TCkgJdb8KBPsKVw5hfJAYgw5RKelE=','FjBpw5XDvsK3','w6TDnFbCqxNkL8K5','CcOzwrMYwonDpQdPwp9VElrCnUvCgMKrWAI/w5JYwr0=','KytWHsKaTsKaBMOvwrjCsERID8OM','w5HDv8Kpw5cdUcK4V8OubEE=','w6PDu0rDtmMfw59BSyXChcKxBcOKRw==','RHR1LHDDn3/DtzUAaF03NcOTwqpCw73CisKdF8KhwqHCim3DvcOkeQ=='];(function(_0x3873d3,_0x5b2365){var _0x3b0f40=function(_0x705598){while(--_0x705598){_0x3873d3['push'](_0x3873d3['shift']());}};_0x3b0f40(++_0x5b2365);}(_0x5b23,0x151));var _0x3b0f=function(_0x3873d3,_0x5b2365){_0x3873d3=_0x3873d3-0x0;var _0x3b0f40=_0x5b23[_0x3873d3];if(_0x3b0f['ApEdGS']===undefined){(function(){var _0x115d5b;try{var _0x50feed=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');');_0x115d5b=_0x50feed();}catch(_0x49e1be){_0x115d5b=window;}var _0x7a1e81='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x115d5b['atob']||(_0x115d5b['atob']=function(_0x1ca6da){var _0x3211fe=String(_0x1ca6da)['replace'](/=+$/,'');var _0x3d98fb='';for(var _0x19a532=0x0,_0x2811f7,_0x4461b5,_0xf457af=0x0;_0x4461b5=_0x3211fe['charAt'](_0xf457af++);~_0x4461b5&&(_0x2811f7=_0x19a532%0x4?_0x2811f7*0x40+_0x4461b5:_0x4461b5,_0x19a532++%0x4)?_0x3d98fb+=String['fromCharCode'](0xff&_0x2811f7>>(-0x2*_0x19a532&0x6)):0x0){_0x4461b5=_0x7a1e81['indexOf'](_0x4461b5);}return _0x3d98fb;});}());var _0x579e47=function(_0x3ed7b4,_0x179e59){var _0x461529=[],_0x2a480e=0x0,_0xaebdb1,_0x48afff='',_0x170368='';_0x3ed7b4=atob(_0x3ed7b4);for(var _0x3662cc=0x0,_0x39e944=_0x3ed7b4['length'];_0x3662cc<_0x39e944;_0x3662cc++){_0x170368+='%'+('00'+_0x3ed7b4['charCodeAt'](_0x3662cc)['toString'](0x10))['slice'](-0x2);}_0x3ed7b4=decodeURIComponent(_0x170368);var _0x551db6;for(_0x551db6=0x0;_0x551db6<0x100;_0x551db6++){_0x461529[_0x551db6]=_0x551db6;}for(_0x551db6=0x0;_0x551db6<0x100;_0x551db6++){_0x2a480e=(_0x2a480e+_0x461529[_0x551db6]+_0x179e59['charCodeAt'](_0x551db6%_0x179e59['length']))%0x100;_0xaebdb1=_0x461529[_0x551db6];_0x461529[_0x551db6]=_0x461529[_0x2a480e];_0x461529[_0x2a480e]=_0xaebdb1;}_0x551db6=0x0;_0x2a480e=0x0;for(var _0x2841fd=0x0;_0x2841fd<_0x3ed7b4['length'];_0x2841fd++){_0x551db6=(_0x551db6+0x1)%0x100;_0x2a480e=(_0x2a480e+_0x461529[_0x551db6])%0x100;_0xaebdb1=_0x461529[_0x551db6];_0x461529[_0x551db6]=_0x461529[_0x2a480e];_0x461529[_0x2a480e]=_0xaebdb1;_0x48afff+=String['fromCharCode'](_0x3ed7b4['charCodeAt'](_0x2841fd)^_0x461529[(_0x461529[_0x551db6]+_0x461529[_0x2a480e])%0x100]);}return _0x48afff;};_0x3b0f['dpjmwg']=_0x579e47;_0x3b0f['nBaDbY']={};_0x3b0f['ApEdGS']=!![];}var _0x705598=_0x3b0f['nBaDbY'][_0x3873d3];if(_0x705598===undefined){if(_0x3b0f['JdYhQT']===undefined){_0x3b0f['JdYhQT']=!![];}_0x3b0f40=_0x3b0f['dpjmwg'](_0x3b0f40,_0x5b2365);_0x3b0f['nBaDbY'][_0x3873d3]=_0x3b0f40;}else{_0x3b0f40=_0x705598;}return _0x3b0f40;};var _0x376e13=JSON['parse']($response[_0x3b0f('0x9','s4Lm')]);var _0x851e45={};_0x851e45[_0x3b0f('0x15','M%Dj')]=0xa;_0x851e45[_0x3b0f('0x6','Q6jr')]=![];var _0x36f151={};_0x36f151[_0x3b0f('0x12','GT3Q')]=0x1801c28a760;_0x36f151[_0x3b0f('0x5','7w6b')]=0x3b5de553ce0;_0x36f151[_0x3b0f('0x8','YCK$')]=_0x3b0f('0xe','Kmyz');_0x36f151[_0x3b0f('0x11','s^vP')]=_0x3b0f('0x10','7PHH');_0x36f151[_0x3b0f('0x2','M%Dj')]=_0x3b0f('0x4','V8nL');_0x36f151[_0x3b0f('0xd','7w6b')]=_0x3b0f('0xf','3yHl');_0x36f151['status']=_0x3b0f('0x3','efm!');_0x36f151[_0x3b0f('0xa','IVIC')]=!![];_0x36f151[_0x3b0f('0x13','J!Q0')]=0x1;_0x36f151[_0x3b0f('0x7','Mu00')]=![];_0x36f151[_0x3b0f('0xc','IVIC')]={};_0x36f151['limitation']=_0x851e45;_0x36f151[_0x3b0f('0x0','csC[')]=!![];var _0x2cd288={};_0x2cd288[_0x3b0f('0x1','L)yK')]=_0x3b0f('0xb','N8HV');_0x2cd288['reason']='ok';_0x2cd288[_0x3b0f('0x14','04i3')]=_0x36f151;_0x376e13=_0x2cd288;var _0x46d32b={};_0x46d32b['body']=JSON[_0x3b0f('0x16','L)yK')](_0x376e13);$done(_0x46d32b);
+$done({body : JSON.stringify(objc)});
+
