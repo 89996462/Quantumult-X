e5251499f2e36ee1eec8b7bb801539ba9a97a634
彭于晏 <65338783+89996462@users.noreply.github.com>
2025-04-22 10:59:27 +0800

    Update iwzbz.js

--- ycdz/iwzbz.js
+++ ycdz/iwzbz.js
@@ -4,7 +4,7 @@
 下载地址：https://t.cn/A60rFcfv
 软件版本：1.2.2
 脚本作者：彭于晏💞
-更新时间：2024-10—8
+更新时间：2025-10—8
 问题反馈：QQ+89996462
 QQ会员群：779392027💞
 TG反馈群：https://t.me/plus8889
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^http[s]?:\/\/bzpp2.iwzbz.com/api/v1.1/user/getvipinfo url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/iwzbz.js
+https:\/\/.+\.iwzbz\.com\/api\/v1\.1\/user\/getvipinfo url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/iwzbz.js.js
 
 [mitm] 
 
@@ -23,6 +23,19 @@ hostname = bzpp2.iwzbz.com
 
 *******************************/
 
-var objc = JSON.parse($response.body);
-    objc = {"code":-1,"data":{"vipTipsType":0,"vipLevel":3,"adtype":0,"expires":"2099-12-31 23:59:59"},"message":"","validateKey":"","isSuccess":true};
-$done({body : JSON.stringify(objc)});
+
+const targetAPI = /https:\/\/.+\.iwzbz\.com\/api\/v1\.1\/user\/getvipinfo/;
+
+if ($request && targetAPI.test($request.url)) {
+  let body = JSON.parse($response.body);
+  if (body.data) {
+    body.isSuccess = true;
+    body.code = 1;
+    body.data.vipLevel = 3;
+    body.data.expires = "9999-09-09";
+  }
+  
+  $done({ body: JSON.stringify(body) });
+} else {
+  $done({});
+}
