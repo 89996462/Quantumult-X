8686fa329a83da443d0713530b575b9a82dc95ce
彭于晏 <65338783+89996462@users.noreply.github.com>
2025-07-08 10:29:51 +0800

    Update dsXy.js

--- ycdz/dsXy.js
+++ ycdz/dsXy.js
@@ -2,11 +2,10 @@
 
 脚本功能：大神水印——解锁VIP
 下载地址：https://is.gd/CzynmP
-软件版本：1.3.0
+软件版本：2.7.4
 脚本作者：彭于晏💞
-更新时间：2023-3-9
+更新时间：2025-8-9
 问题反馈：QQ+89996462
-QQ会员群：779392027💞
 TG反馈群：https://t.me/plus8889
 TG频道群：https://t.me/py996
 使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
@@ -15,11 +14,12 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^http[s]?:\/\/dashen.shuiyinyu.com\/m\/user\/get_user_info url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/dsXy.js
+^https:\/\/mars\.inout\.top\/user\/get_user_info url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/dsXy.js
 
 [mitm] 
 
-hostname = dashen.shuiyinyu.com
+hostname = mars.inout.top
+
 
 *******************************/
 
