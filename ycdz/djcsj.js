2f14758e67782700a9910d426bead0f1d16bdb8c
彭于晏 <65338783+89996462@users.noreply.github.com>
2025-12-31 12:21:48 +0800

    Create djcsj.js

---
+++ ycdz/djcsj.js
@@ -0,0 +1,33 @@
+/******************************
+
+脚本功能：DJ串烧库
+软件版本：v2.0
+脚本作者：彭于晏💞
+更新时间：2025-12-31
+
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+
+*******************************
+
+[rewrite_local]
+
+^http:\/\/csj.yy-5.com:80/api/User/Info url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/djcsj.js
+
+[mitm] 
+
+hostname = csj.yy-5.com
+
+*******************************/
+
+
+var body = $response.body.replace(/isvip":\w+/g,'isvip":true')
+.replace(/expiretime":\d+/g,'expiretime":8888888888')
+.replace(/viptype":\d/g,'viptype":1')
+.replace(/hasvipcode":\w+/g,'hasvipcode":true')
+$done({ body });
+
+
