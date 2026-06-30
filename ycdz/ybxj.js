3e3da1572815a2e9c6ee674bdbf79615fa8cfadd
彭于晏 <65338783+89996462@users.noreply.github.com>
2026-06-30 20:14:51 +0800

    Update ybxj.js

--- ycdz/ybxj.js
+++ ycdz/ybxj.js
@@ -11,7 +11,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^http:\/\/www.wonzeecam.top:80/zeeapp/account/profile/summary url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ybxj.js
+^http:\/\/www.wonzeecam.top/zeeapp/account/profile/summary url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ybxj.js
 
 [mitm] 
 
