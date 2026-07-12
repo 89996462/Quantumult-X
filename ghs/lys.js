0d668edc9559c4e7773049f1f5f912f19bc5e029
彭于晏 <65338783+89996462@users.noreply.github.com>
2026-07-12 21:45:56 +0800

    Update lys.js

--- ghs/lys.js
+++ ghs/lys.js
@@ -1,25 +1,3 @@
-/******************************
-
-脚本功能：萝幼社——解锁—金币视频—VIP视频-去广告-漫画无解
-特别说明：开启脚本即可在线观看视频
-特别说明：必须开启HTTP抓包,并且关闭其他的脚本
-更新时间：2026—7-14
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py996
-使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
-
-*******************************
-
-[rewrite_local]
-
-^https?:\/\/oihqwlma\.00po\.shop\/api\/app\/vid\/m3u8\/.* url script-request-header https://raw.githubusercontent.com/89996462/Quantumult-X/main/ghs/lys.js
-
-[mitm]
-
-hostname = oihqwlma.00po.shop, *.00po.shop, *.oukky-lys.shop, *.cyou, *.eqfx9bas.cc, *.yihaici.top, *.yihaici.com, *.epuf3tk.cc
-
-*******************************/
-
 const headers = $request.headers;
 const url = $request.url;
 if (url.includes('/api/app/vid/m3u8/') && url.includes('.m3u8')) {
